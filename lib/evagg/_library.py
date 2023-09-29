536d6731cd5d87cc1c694c06f4d879eb1cb7e8ee
Miah Wander <miah@microsoft.com>
2023-09-29 03:00:48 +0000

    linting yay

--- lib/evagg/_library.py
+++ lib/evagg/_library.py
@@ -1,15 +1,13 @@
-from typing import Sequence
-from functools import cache
-import os
 import json
+import os
+from functools import cache
+from typing import Sequence
 
+from ._base import Paper, Variant
 from ._interfaces import IGetPapers
-from ._base import Variant, Paper
-
 
 
 class SimpleFileLibrary(IGetPapers):
-
     def __init__(self, collections: Sequence[str]) -> None:
         self._collections = collections
 
@@ -17,22 +15,22 @@ class SimpleFileLibrary(IGetPapers):
         papers = {}
         # collection is a local directory, get a list of all of the json files in that directory
         for filename in os.listdir(collection):
-            if filename.endswith('.json'):
+            if filename.endswith(".json"):
                 # load the json file into a dict and append it to papers
-                with open(os.path.join(collection, filename), 'r') as f:
+                with open(os.path.join(collection, filename), "r") as f:
                     paper = Paper.from_dict(json.load(f))
                     papers[paper.id] = paper
         return papers
-    
+
     @cache
     def _load(self) -> dict[str, Paper]:
         papers = {}
         for collection in self._collections:
             papers.update(self._load_collection(collection))
-        
+
         return papers
 
     def search(self, query: Variant) -> Sequence[Paper]:
         # Dummy implementation that returns all papers regardless of query.
-        all_papers = self._load().values()
+        all_papers = list(self._load().values())
         return all_papers
