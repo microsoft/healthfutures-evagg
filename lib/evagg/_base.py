433ccb05d79dfb3d2ee95a3362324d3c6a0222c3
Greg Smith <gregsmi@microsoft.com>
2023-10-10 07:21:28 -0700

    extra paper props, query abstraction

--- lib/evagg/_base.py
+++ lib/evagg/_base.py
@@ -1,20 +1,20 @@
 # TODO dataclass?
 # TODO should be immutable after load.
+from typing import Any
+
+
 class Paper:
-    def __init__(self, id: str, citation: str, abstract: str) -> None:
-        self.id = id
-        self.citation = citation
-        self.abstract = abstract
+    def __init__(self, **kwargs: Any) -> None:
+        self.id = kwargs.pop("id")
+        self.citation = kwargs.pop("citation", "")
+        self.abstract = kwargs.pop("abstract", "")
+        self.data = kwargs
 
     def __repr__(self) -> str:
         m = 10
         asuf = "..." if len(self.abstract) > m else ""
 
         return f'id: {self.id} - abstract: "{self.abstract[:m]}{asuf}"'
-
-    @classmethod
-    def from_dict(cls, values: dict[str, str]) -> "Paper":
-        return Paper(id=values["id"], citation=values["citation"], abstract=values["abstract"])
 
 
 class Variant:
