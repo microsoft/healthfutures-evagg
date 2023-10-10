86d1e0f1bf9c4482281a379f88f02f446cb20aee
Greg Smith <gregsmi@microsoft.com>
2023-10-10 13:20:23 -0700

    updated Paper structure

--- lib/evagg/_base.py
+++ lib/evagg/_base.py
@@ -5,10 +5,18 @@ from typing import Any
 
 class Paper:
     def __init__(self, **kwargs: Any) -> None:
-        self.id = kwargs.pop("id")
+        self.id = kwargs.pop("id")  # id is required
         self.citation = kwargs.pop("citation", "")
         self.abstract = kwargs.pop("abstract", "")
         self.data = kwargs
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Paper):
+            return False
+        return self.id == o.id
 
     def __repr__(self) -> str:
         m = 10
