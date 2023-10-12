50e47eecac38efb279979eb09cb318a55fa0f5e9
Miah Wander <miah@microsoft.com>
2023-10-12 20:23:27 +0000

    fix broken tests

--- test/evagg/test_base.py
+++ test/evagg/test_base.py
@@ -3,17 +3,18 @@ from lib.evagg import Paper
 
 def test_repr():
     # Test case 1: abstract length is less than 10
-    obj = Paper(id="1", citation="", abstract="hello")
+    obj = Paper(id="1", citation="", abstract="hello", pmcid="PMC123")
     assert repr(obj) == 'id: 1 - abstract: "hello"'
 
     # Test case 2: abstract length is greater than 10
-    obj = Paper(id="2", citation="", abstract="hello world!")
+    obj = Paper(id="2", citation="", abstract="hello world!", pmcid="PMC123")
     assert repr(obj) == 'id: 2 - abstract: "hello worl..."'
 
 
 def test_paper_from_dict():
-    paper_dict = {"id": "123", "citation": "Test Citation", "abstract": "Test Abstract"}
+    paper_dict = {"id": "123", "citation": "Test Citation", "abstract": "Test Abstract", "pmcid": "PMC123"}
     paper = Paper.from_dict(paper_dict)
     assert paper.id == "123"
     assert paper.citation == "Test Citation"
     assert paper.abstract == "Test Abstract"
+    assert paper.pmcid == "PMC123"
--- test/evagg/test_content.py
+++ test/evagg/test_content.py
@@ -1,11 +1,14 @@
-from lib.evagg import Paper, SimpleContentExtractor
+from lib.evagg import Paper, Query, SimpleContentExtractor
 
 
 def test_simple_content_extractor():
-    paper = Paper(id="12345678", citation="Doe, J. et al. Test Journal 2021", abstract="This is a test paper.")
+    paper = Paper(
+        id="12345678", citation="Doe, J. et al. Test Journal 2021", abstract="This is a test paper.", pmcid="PMC123"
+    )
+    query = Query(gene="CHI3L1", variant="p.Y34C")
     fields = ["gene", "variant", "MOI", "phenotype", "functional data"]
     extractor = SimpleContentExtractor(fields)
-    result = extractor.extract(paper)
+    result = extractor.extract(query=query, paper=paper)
     assert len(result) == 1
     assert result[0]["gene"] == "CHI3L1"
     assert result[0]["variant"] == "p.Y34C"
