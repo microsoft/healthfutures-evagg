7a44a90b92b394f266f2e78c8cfdd3ff8f426946
Miah Wander <miah@microsoft.com>
2023-10-18 15:53:37 +0000

    remove old script

--- lib/scripts/run_single_query_sync/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Package defining run_single_query_sync.
-
-Takes as input a json configuration file and runs the evidence aggregation pipeline synchronously.
-"""
-
-from ._cli import main
-
-__all__ = ["main"]
