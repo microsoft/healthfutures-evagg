c862899c0f490b142789ff699e4305b0dc563cbd
Miah Wander <miah@microsoft.com>
2023-11-01 21:47:40 +0000

    Minimal support for litvar api

--- lib/evagg/ref/__init__.py
+++ lib/evagg/ref/__init__.py
@@ -1,5 +1,12 @@
 """Package for interacting with reference resources."""
 
+from ._litvar import LitVarReference
 from ._ncbi import NCBIGeneReference, NCBIVariantReference
 
-__all__ = ["NCBIGeneReference", "NCBIVariantReference"]
+__all__ = [
+    # Litvar.
+    "LitVarReference",
+    # NCBI.
+    "NCBIGeneReference",
+    "NCBIVariantReference",
+]
