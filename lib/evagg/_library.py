8c8eb4b0f328c216f5d29f418aa19aa9b23c2d26
Ubuntu <azureuser@evagg-ash-1.j2bygn51xe0uvnvtlsf1wcgjoc.bx.internal.cloudapp.net>
2023-11-06 16:04:21 +0000

    added Papers content extraction scripts

--- lib/evagg/_library.py
+++ lib/evagg/_library.py
@@ -1,9 +1,9 @@
-import csv
-import json
-import os
+import csv, json, os, re
 from collections import defaultdict
 from functools import cache
 from typing import Dict, Sequence, Set
+from Bio import Entrez # Biopython
+import xml.etree.ElementTree as ET
 
 from lib.evagg.types import IPaperQuery, Paper, Variant
 
@@ -115,17 +115,12 @@ class PubMedFileLibrary(IGetPapers): # TODO: consider gene:variant info next
         Entrez.email = email
         self._max_papers = max_papers
     
-    def search(self, query: IPaperQuery) -> Set[Paper]:
-        # take query.terms
+    def search_pubmed(self, query: IPaperQuery) -> Set[Paper]:
         term = query.terms()[0]
-        id_list = self._search_id_for_gene(gene=term[:term.find(":")]) # returns gene string
+        id_list = self._find_ids_for_gene(gene=term[:term.find(":")])
         return self._build_papers(id_list)
     
-        # find papers (PMIDs?)
-        
-        return None
-    
-    def _search_id_for_gene(self, query):
+    def _find_ids_for_gene(self, query):
         handle = Entrez.esearch(db='pmc', 
                                 sort='relevance', 
                                 retmax= self._max_papers,
@@ -134,51 +129,78 @@ class PubMedFileLibrary(IGetPapers): # TODO: consider gene:variant info next
         id_list = Entrez.read(handle)['IdList']
         return id_list
 
-    def _fetch_details(self, id_list: Sequence[str]) -> Dict[str, Dict[str, str]]:
+    def _fetch_parse_xml(self, id_list: Sequence[str]) -> Dict[str, Dict[str, str]]:
         ids = ','.join(id_list)
         handle = Entrez.efetch(db='pmc',
                             retmode='xml',
                             id=ids)
-        results = Entrez.read(handle)
+        #results = Entrez.read(handle)
+        tree = ET.parse(handle)
+        root = tree.getroot()
+        all_xml_elements = list(root.iter())
+
+        list_dois = []
+        for elem in all_xml_elements:
+            if((elem.tag == "pub-id") and ("/" in str(elem.text))==True):
+                list_dois.append(elem.text)
+        return(list_dois, all_xml_elements) # returns DOI, XML
         
         # For each paper (DOI), we want to know PMCID, Abstract, Citation.
-        return_dict = {}
-        for result in results:
-            return_dict[result.doi] = {
-                "abstract": result.abstract,
-                "citation": result.citation,
-                "pmcid": result.pmcid
-            }
-        # TODO: return a dict key:"DOI", dicts everything else about the paper ()
-        # those would get passed to the contructor of paper
-        return return_dict
+        # return_dict = {}
+        # for result in results:
+        #     return_dict[result.doi] = {
+        #         "abstract": result.abstract,
+        #         "citation": result.citation,
+        #         "pmcid": result.pmcid
+        #     }
+        # # TODO: return a dict key:"DOI", dicts everything else about the paper ()
+        # # those would get passed to the contructor of paper
+        # return return_dict
     
-    def _find_DOIs_in_text(self, text):
-        pattern = r"StringElement\('(\d+)', attributes=\{'pub-id-type': 'doi'\}\)"
-        matches = re.findall(pattern, text)
-
-        # Print all matches (PMIDs)
-        for match in matches:
-            print("DOI:", match)
+    def _find_pmid_in_xml(all_xml_elements):
+        list_pmids = []
+        for elem in all_elements:
+            if((elem.tag == "pub-id") and ("/" not in str(elem.text))==True): # doi
+                list_pmids.append(elem.text)
+        return(list_pmids) # returns PMIDs
             
-    def _find_PMCIDs_in_text(self, text):
-        pattern = r"StringElement\('(\d+)', attributes=\{'pub-id-type': 'pmc'\}\)"
-        matches = re.findall(pattern, text)
+    def _get_abstract_and_citation(pmid):
+        handle = Entrez.efetch(db='pubmed', id=pmid, retmode='text', rettype='abstract')
+        text_info = handle.read()
+        citation = _generate_citation(text_info)
+        abstract = _extract_abstract(text_info)
+        return(citation, abstract)
 
-        # Print all matches (PMIDs)
-        for match in matches:
-            print("PMID:", match)
-            
+    def _generate_citation(text_info):
+        # Extract the author's last name
+        match = re.search(r'(\n\n[^\.]*\.)\n\nAuthor information', text_info, re.DOTALL)
+        sentence = match.group(1).replace('\n', ' ')
+        author_lastname = sentence.split()[0]
+        
+        # Extract year, journal abbr., DOI 
+        year = re.search(r'\. (\d{4}) ', text_info).group(1) # Extract pub. year
+        journal_abbr = re.search(r'\. ([^\.]*\.)', text_info).group(1).strip(".") # Extract journal abbreviation
+        doi_number = re.search(r'\nDOI: (.*)\nPMID', text_info).group(1).replace('10.1111/', '').strip() # Extract DOI number, TODO: modify to pull from key
+        
+        citation = f"{author_lastname} ({year}), {journal_abbr}., {doi_number}" # Construct citation
+        return citation
+
+    def _extract_abstract(text_info):
+        abstract = re.search(r'Author information:.*?\.(.*)DOI:', text_info, re.DOTALL).group(1).strip() # Extract paragraph after "Author information:" sentence and before "DOI:"
+        return abstract
+
     def _build_papers(self, id_list):
         papers: List[Paper] = []
-        papers_xml = self._fetch_details(id_list)
+        list_dois, papers_xml = self._fetch_parse_xml(id_list)
+        list_pmids = self._find_pmid_in_xml(id_list)
+        for pmid in list_pmids:
+           citation, abstract = _get_abstract_and_citation(pmid)
+        
         for key, value in papers_xml.values():
             papers.append(Paper(id=key, **value))
         #dois = self._find_DOIs_in_text(str(papers_xml))
         #pmcids = self._find_PMCIDs_in_text(str(papers_xml))
-        return papers
-        
-        return 
+        return papers 
         
     
 class HanoverFileLibrary(IGetPapers):
