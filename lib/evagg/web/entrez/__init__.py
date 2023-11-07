fde1e612c3ceaa5950127e93f4899944a751fb9d
Miah Wander <miah@microsoft.com>
2023-11-07 18:14:07 +0000

    allow api key

--- lib/evagg/web/entrez/__init__.py
+++ lib/evagg/web/entrez/__init__.py
@@ -1,6 +1,13 @@
 """Classes for interacting with Entrez APIs."""
 
-from ._client import BioEntrezClient
+from ._client import BioEntrezClient, BioEntrezConfig, BioEntrezDotEnvConfig
 from ._interfaces import IEntrezClient
 
-__all__ = ["IEntrezClient", "BioEntrezClient"]
+__all__ = [
+    # Interfaces.
+    "IEntrezClient",
+    # Client.
+    "BioEntrezClient",
+    "BioEntrezDotEnvConfig",
+    "BioEntrezConfig",
+]
