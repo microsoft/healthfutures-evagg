ca3b7d5d0ec1adbfa443d23b2e852bc410976713
Miah Wander <miah@microsoft.com>
2023-11-07 22:18:32 +0000

    max_tries parameter

--- lib/evagg/web/entrez/_interfaces.py
+++ lib/evagg/web/entrez/_interfaces.py
@@ -1,5 +1,7 @@
 from typing import Protocol
 
+# TODO, this should really accept a list of IDs, and return a list of results.
+
 
 class IEntrezClient(Protocol):
     def efetch(self, db: str, id: str, retmode: str | None = None, rettype: str | None = None) -> str:
