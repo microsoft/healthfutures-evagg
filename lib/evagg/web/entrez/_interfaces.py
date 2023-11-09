1f17b21c945b35b501482faf2af4962903839e9f
Miah Wander <miah@microsoft.com>
2023-11-09 00:40:16 +0000

    xml based multiple rsids

--- lib/evagg/web/entrez/_interfaces.py
+++ lib/evagg/web/entrez/_interfaces.py
@@ -1,8 +1,7 @@
 from typing import Protocol
 
-# TODO, this should really accept a list of IDs, and return a list of results.
-
 
 class IEntrezClient(Protocol):
     def efetch(self, db: str, id: str, retmode: str | None = None, rettype: str | None = None) -> str:
+        """Call the Entrez EFetch API. `id` can be a comma-separated list of IDs."""
         ...
