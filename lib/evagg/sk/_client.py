eb311db0df5285844447bff874ca37ae0c8e2f4a
Miah Wander <miah@microsoft.com>
2023-12-13 20:11:24 +0000

    demo readiness

--- lib/evagg/sk/_client.py
+++ lib/evagg/sk/_client.py
@@ -41,13 +41,24 @@ class SemanticKernelDotEnvConfig(SemanticKernelConfig):
         )
 
 
+import logging
+
+
 class SemanticKernelClient:
     DEFAULT_SKILL_DIR = os.path.join(os.path.dirname(__file__), "skills")
     _skill_dir: str = DEFAULT_SKILL_DIR
     _functions: Dict[Tuple[str, str], sk.SKFunctionBase]
+    _logger: logging.Logger
 
-    def __init__(self, config: SemanticKernelConfig) -> None:
-        self._kernel = sk.Kernel()
+    def __init__(self, config: SemanticKernelConfig, verbose: bool = False) -> None:
+        if verbose:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.WARN)
+
+        self._logger = logging.getLogger(__name__)
+        self._kernel = sk.Kernel(log=self._logger)
+
         self._functions = {}
         # Assume AOAI.
         self._kernel.add_text_completion_service(
@@ -78,6 +89,7 @@ class SemanticKernelClient:
     def _invoke_function(
         self, sk_function: sk.SKFunctionBase, input: str | None, variables: sk.ContextVariables
     ) -> str:
+        self._logger.info(f"Running function {sk_function.name}")
         result = asyncio.run(self._kernel.run_async(sk_function, input_vars=variables, input_str=input))
         if result.error_occurred:
             raise ValueError(f"Error: {result.last_error_description}")
@@ -88,6 +100,7 @@ class SemanticKernelClient:
         # TODO handle token limits?
 
         function_obj = self._get_function(skill, function)
-        input = context_variables.pop("input", None)
+        # input = context_variables.pop("input", None)
+        input = None
         vars = sk.ContextVariables(variables=context_variables)
         return self._invoke_function(function_obj, input, vars)
