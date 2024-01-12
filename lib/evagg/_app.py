f58d66945485b896f490f8ab16520d523300f78e
Greg Smith <gregsmi@microsoft.com>
2024-01-12 10:17:35 -0800

    removed SynchronousLocalApp and MultiQuery

--- lib/evagg/_app.py
+++ lib/evagg/_app.py
@@ -1,29 +1,8 @@
 from typing import Dict, List
 
-from lib.evagg.types import IPaperQuery, IPaperQueryIterator
+from lib.evagg.types import IPaperQueryIterator
 
 from ._interfaces import IEvAggApp, IExtractFields, IGetPapers, IWriteOutput
-
-
-class SynchronousLocalApp(IEvAggApp):
-    def __init__(
-        self, query: IPaperQuery, library: IGetPapers, extractor: IExtractFields, writer: IWriteOutput
-    ) -> None:
-        self._query = query
-        self._library = library
-        self._extractor = extractor
-        self._writer = writer
-
-    def execute(self) -> None:
-        # Get the papers that match the query.
-        papers = self._library.search(self._query)
-        print(f"Found {len(papers)} papers for {self._query.terms()}")
-
-        # For all papers that match, extract the fields we want.
-        fields = {paper.id: self._extractor.extract(paper, self._query) for paper in papers}
-
-        # Write out the result.
-        self._writer.write(fields)
 
 
 class SynchronousLocalBatchApp(IEvAggApp):
--- lib/evagg/types/__init__.py
+++ lib/evagg/types/__init__.py
@@ -2,14 +2,13 @@
 
 from ._base import Paper, Variant
 from ._interfaces import IPaperQuery, IPaperQueryIterator
-from ._query import MultiQuery, Query, QueryIterator
+from ._query import Query, QueryIterator
 
 __all__ = [
     # Base.
     "Paper",
     "Variant",
     # Query.
-    "MultiQuery",
     "Query",
     "QueryIterator",
     # Interfaces.
