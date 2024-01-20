bbe784a55176b13d312d712cdad19505f79515ae
Greg Smith <gregsmi@microsoft.com>
2024-01-20 15:12:14 -0800

    reorg ncbi

--- lib/evagg/ref/ncbi.py
+++ lib/evagg/ref/ncbi.py
@@ -8,6 +8,81 @@ from lib.evagg.web.entrez import IEntrezClient
 from .interfaces import IGeneLookupClient, IVariantLookupClient
 
 
+def _validate_snp_response_xml(response: Any) -> bool:
+    if not response.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}ExchangeSet":
+        return False
+    if len(response) < 1:
+        return False
+    for child in response:
+        if not child.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}DocumentSummary":
+            return False
+    return True
+
+
+def _find_hgvs_xml(response: Any, id: str) -> Dict[str, str]:
+    result = {}
+
+    for child in response:
+        if not child.get("uid") == id:
+            continue
+
+        for nested_child in child:
+            if not nested_child.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}DOCSUM":
+                continue
+
+            if not nested_child.text:
+                break
+
+            values = {}
+            for tok in nested_child.text.split("|"):
+                eq_delim = tok.split("=")
+                if len(eq_delim) != 2:
+                    continue
+                values[eq_delim[0]] = eq_delim[1]
+
+            if "HGVS" not in values:
+                break
+
+            hgvs_values = values["HGVS"].split(",")
+
+            # Just take the first one.
+            hgvs_p = next((v for v in hgvs_values if v.startswith("NP_")), None)
+            hgvs_c = next((v for v in hgvs_values if v.startswith("NM_")), None)
+
+            if hgvs_p:
+                result["hgvs_p"] = hgvs_p
+            if hgvs_c:
+                result["hgvs_c"] = hgvs_c
+            break
+    return result
+
+
+def _extract_gene_data(raw: Dict[str, Any], symbols: Sequence[str], allow_synonyms: bool) -> Dict[str, int]:
+    if "reports" not in raw:
+        return {}
+
+    if allow_synonyms:
+        result: Dict[str, int] = {}
+
+        for g in raw["reports"]:
+            if g["gene"]["symbol"] in symbols:
+                result[g["gene"]["symbol"]] = int(g["gene"]["gene_id"])
+
+        missing_symbols = [s for s in symbols if s not in result.keys()]
+        for symbol in missing_symbols:
+            # find the first query match.
+            for g in raw["reports"]:
+                if symbol in g["query"]:
+                    result[symbol] = int(g["gene"]["gene_id"])
+                    break
+    else:
+        result = {
+            g["gene"]["symbol"]: int(g["gene"]["gene_id"]) for g in raw["reports"] if g["gene"]["symbol"] in symbols
+        }
+
+    return result
+
+
 class NcbiGeneClient(IGeneLookupClient):
     def __init__(self, entrez_client: IEntrezClient) -> None:
         self._entrez_client = entrez_client
@@ -36,92 +111,20 @@ class NcbiGeneClient(IGeneLookupClient):
         url = f"https://api.ncbi.nlm.nih.gov/datasets/v2alpha/gene/symbol/{','.join(symbols)}/taxon/Human"
         raw = cls._get_json(url)
 
-        if "reports" not in raw:
-            return {}
-
-        if allow_synonyms:
-            result: Dict[str, int] = {}
-
-            for g in raw["reports"]:
-                if g["gene"]["symbol"] in symbols:
-                    result[g["gene"]["symbol"]] = int(g["gene"]["gene_id"])
-
-            missing_symbols = [s for s in symbols if s not in result.keys()]
-            for symbol in missing_symbols:
-                # find the first query match.
-                for g in raw["reports"]:
-                    if symbol in g["query"]:
-                        result[symbol] = int(g["gene"]["gene_id"])
-                        break
-        else:
-            result = {
-                g["gene"]["symbol"]: int(g["gene"]["gene_id"]) for g in raw["reports"] if g["gene"]["symbol"] in symbols
-            }
-
-        return result
+        return _extract_gene_data(raw, symbols, allow_synonyms)
 
 
 class NcbiSnpClient(IVariantLookupClient):
     def __init__(self, entrez_client: IEntrezClient) -> None:
         self._entrez_client = entrez_client
 
-    def _entrez_fetch(self, db: str, id_str: str, retmode: str | None, rettype: str | None) -> str:
-        return self._entrez_client.efetch(db=db, id=id_str, retmode=retmode, rettype=rettype)
-
     def _entrez_fetch_xml(self, db: str, id_str: str) -> Any | None:
-        string_response = self._entrez_fetch(db=db, id_str=id_str, retmode="xml", rettype="xml")
+        string_response = self._entrez_client.efetch(db=db, id=id_str, retmode="xml", rettype="xml")
         if len(string_response) == 0:
             return None
         # fromstring returns type Any, but it's actually an ElementTree.Element. Using this approach to avoid
         # making bandit cranky with xml.* imports.
         return ElementTree.fromstring(string_response)
-
-    def _validate_snp_response_xml(self, response: Any) -> bool:
-        if not response.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}ExchangeSet":
-            return False
-        if len(response) < 1:
-            return False
-        for child in response:
-            if not child.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}DocumentSummary":
-                return False
-        return True
-
-    def _find_hgvs_xml(self, response: Any, id: str) -> Dict[str, str]:
-        result = {}
-
-        for child in response:
-            if not child.get("uid") == id:
-                continue
-
-            for nested_child in child:
-                if not nested_child.tag == "{https://www.ncbi.nlm.nih.gov/SNP/docsum}DOCSUM":
-                    continue
-
-                if not nested_child.text:
-                    break
-
-                values = {}
-                for tok in nested_child.text.split("|"):
-                    eq_delim = tok.split("=")
-                    if len(eq_delim) != 2:
-                        continue
-                    values[eq_delim[0]] = eq_delim[1]
-
-                if "HGVS" not in values:
-                    break
-
-                hgvs_values = values["HGVS"].split(",")
-
-                # Just take the first one.
-                hgvs_p = next((v for v in hgvs_values if v.startswith("NP_")), None)
-                hgvs_c = next((v for v in hgvs_values if v.startswith("NM_")), None)
-
-                if hgvs_p:
-                    result["hgvs_p"] = hgvs_p
-                if hgvs_c:
-                    result["hgvs_c"] = hgvs_c
-                break
-        return result
 
     def hgvs_from_rsid(self, rsids: Sequence[str]) -> Dict[str, Dict[str, str]]:
         """Provided rsids should be a list of strings, each of which is a valid rsid, prefixed with `rs`."""
@@ -143,10 +146,10 @@ class NcbiSnpClient(IVariantLookupClient):
         rsids_str = ",".join(keys)
         response = self._entrez_fetch_xml(db="snp", id_str=rsids_str)
 
-        if response is None or not self._validate_snp_response_xml(response):
+        if response is None or not _validate_snp_response_xml(response):
             return {}
 
         result = {}
         for rsid in keys:
-            result["rs" + rsid] = self._find_hgvs_xml(response, rsid)
+            result["rs" + rsid] = _find_hgvs_xml(response, rsid)
         return result
