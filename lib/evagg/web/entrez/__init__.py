7ff0f1e817b915f7899cc92ac99a861f258418f6
Greg Smith <gregsmi@microsoft.com>
2024-01-22 16:38:03 -0800

    removed BioEntrezClient

--- lib/evagg/web/entrez/__init__.py
+++ lib/evagg/web/entrez/__init__.py
@@ -1,13 +1,8 @@
 """Classes for interacting with Entrez APIs."""
 
-from .client import BioEntrezClient, BioEntrezConfig, BioEntrezDotEnvConfig
 from .interfaces import IEntrezClient
 
 __all__ = [
     # Interfaces.
     "IEntrezClient",
-    # Client.
-    "BioEntrezClient",
-    "BioEntrezDotEnvConfig",
-    "BioEntrezConfig",
 ]
