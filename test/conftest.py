50214fdfe005855e3fe60893be76b88106d54ffe
Greg Smith <gregsmi@microsoft.com>
2024-01-24 13:59:48 -0800

    added test file fixtures

---
+++ test/conftest.py
@@ -0,0 +1,28 @@
+import json
+import os
+import xml.etree.ElementTree as Et
+
+import pytest
+
+
+@pytest.fixture
+def test_resources_path():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
+
+
+@pytest.fixture
+def json_load(test_resources_path):
+    def _loader(file_name):
+        with open(os.path.join(test_resources_path, file_name), "r") as json_file:
+            return json.load(json_file)
+
+    return _loader
+
+
+@pytest.fixture
+def xml_load(test_resources_path):
+    def _loader(file_name):
+        with open(os.path.join(test_resources_path, file_name), "r") as xml_file:
+            return Et.fromstring(xml_file.read())
+
+    return _loader
