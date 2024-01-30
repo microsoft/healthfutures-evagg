c12da91aa4f40e7b8a4c0ee40c0a53ffa08801e7
Greg Smith <gregsmi@microsoft.com>
2024-01-30 14:12:15 -0800

    added retries, settings, and tests to web client

---
+++ test/evagg/test_web.py
@@ -0,0 +1,63 @@
+import json
+from unittest.mock import MagicMock, patch
+
+import requests
+from pytest import raises
+
+from lib.evagg.svc import RequestsWebContentClient
+
+
+def test_settings():
+    web_client = RequestsWebContentClient()
+    web_client.set_session_defaults(max_retries=1, retry_backoff=2, retry_codes=[500, 429], content_type="json")
+    settings = web_client._defaults.dict()
+    assert settings["max_retries"] == 1
+    assert settings["retry_backoff"] == 2
+    assert settings["retry_codes"] == [500, 429]
+    assert settings["content_type"] == "json"
+
+    with raises(ValueError):
+        web_client.set_session_defaults(invalid=1)
+
+
+def test_content_types():
+    web_client = RequestsWebContentClient()
+    assert web_client._get_content(MagicMock(text="test"), "text") == "test"
+    assert web_client._get_content(MagicMock(content=b"test"), "binary") == b"test"
+    assert web_client._get_content(MagicMock(content=b"<test>1</test>"), "xml").tag == "test"
+    assert web_client._get_content(
+        MagicMock(content=b'{"test": 1}', json=lambda: json.loads('{"test": 1}')), "json"
+    ) == {"test": 1}
+
+    with raises(ValueError):
+        RequestsWebContentClient(default_content_type="invalid")
+
+
+@patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
+def test_retry_succeeded(mock_get_conn):
+    mock_get_conn.return_value.getresponse.side_effect = [
+        MagicMock(status=500, headers={}, iter_content=lambda _: [b""]),
+        MagicMock(status=429, headers={}, iter_content=lambda _: [b""]),
+        MagicMock(status=200, headers={}, iter_content=lambda _: [b""]),
+    ]
+
+    web_client = RequestsWebContentClient()
+    web_client.set_session_defaults(max_retries=2, retry_backoff=0, retry_codes=[500, 429])
+    web_client.get("https://any.url/testing")
+
+    assert mock_get_conn.return_value.request.call_args.args[0] == "GET"
+    assert mock_get_conn.return_value.request.call_args.args[1] == "/testing"
+    assert len(mock_get_conn.return_value.request.mock_calls) == 3
+
+
+@patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
+def test_retry_failed(mock_get_conn):
+    mock_get_conn.return_value.getresponse.side_effect = [
+        MagicMock(status=429, headers={}, iter_content=lambda _: [b""]),
+        MagicMock(status=500, headers={}, iter_content=lambda _: [b""]),
+    ]
+
+    web_client = RequestsWebContentClient()
+    web_client.set_session_defaults(max_retries=1, retry_backoff=0, retry_codes=[500, 429])
+    with raises(requests.exceptions.RetryError):
+        web_client.get("https://any.url/testing")
