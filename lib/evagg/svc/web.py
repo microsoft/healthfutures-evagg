8fc55187c0f5c33a4a5b724f08736b46ec69dbbe
Greg Smith <gregsmi@microsoft.com>
2024-01-30 18:33:55 -0800

    overhauled settings

--- lib/evagg/svc/web.py
+++ lib/evagg/svc/web.py
@@ -1,8 +1,9 @@
 import logging
 import xml.etree.ElementTree as Et
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
 import requests
+from pydantic import Extra, validator
 from requests.adapters import HTTPAdapter, Retry
 
 from lib.config import PydanticYamlModel
@@ -14,35 +15,35 @@ logger = logging.getLogger(__name__)
 CONTENT_TYPES = ["text", "binary", "json", "xml"]
 
 
-def _content_type_check(content_type: Optional[str], default: str) -> str:
-    content_type = (content_type or default).lower()
-    if content_type not in CONTENT_TYPES:
-        raise ValueError(f"Web content type must be one of {'/'.join(CONTENT_TYPES)}, got '{content_type}'")
-    return content_type
-
-
-class WebClientSettings(PydanticYamlModel):
-    max_retries: int = 0
-    retry_backoff: float = 0.5
-    retry_codes: List[int] = [502, 503, 504]
+class WebClientSettings(PydanticYamlModel, extra=Extra.forbid):
+    max_retries: int = 0  # no retries by default
+    retry_backoff: float = 0.5  # indicates progression of 0.5, 1, 2, 4, 8, etc. seconds
+    retry_codes: List[int] = [429, 500, 502, 503, 504]  # rate-limit exceeded, server errors
     content_type: str = "text"
+
+    @validator("content_type")
+    @classmethod
+    def _validate_content_type(cls, value: str) -> str:
+        if value not in CONTENT_TYPES:
+            raise ValueError(f"Web content type must be one of {'/'.join(CONTENT_TYPES)}, got '{value}'")
+        return value
 
 
 class RequestsWebContentClient(IWebContentClient):
     """A web content client that uses the requests/urllib3 libraries."""
 
-    def __init__(self, default_content_type: Optional[str] = None) -> None:
+    def __init__(self, settings: Optional[Dict[str, Any]] = None) -> None:
+        self._settings = WebClientSettings(**settings) if settings else WebClientSettings()
         self._session: Optional[requests.Session] = None
-        self._defaults = WebClientSettings(content_type=_content_type_check(default_content_type, "text"))
 
     def _get_session(self) -> requests.Session:
         """Get the session, initializing it if necessary."""
         if self._session is None:
             self._session = requests.Session()
             retries = Retry(
-                total=self._defaults.max_retries,
-                backoff_factor=self._defaults.retry_backoff,
-                status_forcelist=self._defaults.retry_codes,
+                total=self._settings.max_retries,
+                backoff_factor=self._settings.retry_backoff,
+                status_forcelist=self._settings.retry_codes,
             )
             self._session.mount("https://", HTTPAdapter(max_retries=retries))
             self._session.mount("http://", HTTPAdapter(max_retries=retries))
@@ -50,7 +51,7 @@ class RequestsWebContentClient(IWebContentClient):
 
     def _get_content(self, response: requests.Response, content_type: Optional[str]) -> Any:
         """Get the content from the response based on the provided content type."""
-        content_type = _content_type_check(content_type, self._defaults.content_type)
+        content_type = content_type or self._settings.content_type
         if content_type == "text":
             return response.text
         elif content_type == "binary":
@@ -62,12 +63,10 @@ class RequestsWebContentClient(IWebContentClient):
         else:
             raise ValueError(f"Invalid content type: {content_type}")
 
-    def set_session_defaults(self, **kwargs: Any) -> None:
-        """Set the default values for the session."""
-        for key, value in kwargs.items():
-            if key not in self._defaults.dict().keys():
-                raise ValueError(f"Invalid session default: {key}")
-            setattr(self._defaults, key, value)
+    def update_settings(self, **kwargs: Any) -> None:
+        """Update the default values for the session."""
+        updated_settings = {**self._settings.dict(), **kwargs}
+        self._settings = WebClientSettings(**updated_settings)
 
     def get(self, url: str, content_type: Optional[str] = None) -> Any:
         """GET the content at the provided URL."""
