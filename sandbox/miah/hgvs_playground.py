ba78f6b2060fd4f46a372e62a208bcd0cfe1b55b
Miah Wander <miah@microsoft.com>
2024-01-31 16:38:44 +0000

    wip, env update

--- sandbox/miah/hgvs_playground.py
+++ sandbox/miah/hgvs_playground.py
@@ -6,15 +6,20 @@
 
 # %% Imports.
 
-from lib.evagg.lit.pubmed import PubtatorEntityAnnotator
-from lib.evagg.types import Paper
+import re
+from typing import Any, Dict, List, Sequence
 
-from typing import Sequence, Dict, List
 import requests
+
+from lib.evagg.lit.pubmed import PubtatorEntityAnnotator
+from lib.evagg.ref import NcbiGeneClient
+from lib.evagg.types import Paper
+from lib.evagg.web.entrez import BioEntrezClient, BioEntrezDotEnvConfig
 
 # %% Definitions.
 
-def gene_symbols_for_id(gene_ids: Sequence[str]) -> Dict[str, List[str]]:
+
+def gene_symbols_for_id(gene_ids: Sequence[str], max: int = -1) -> Dict[str, List[str]]:
     seq_str = ",".join(gene_ids)
 
     url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id={seq_str}&format=json"
@@ -26,13 +31,14 @@ def gene_symbols_for_id(gene_ids: Sequence[str]) -> Dict[str, List[str]]:
 
     for gene_id in gene_ids:
         gene = response_dict["result"].get(gene_id, None)
-        print(gene)
         if gene:
-            symbols[gene_id] = [gene["name"]] + gene["otheraliases"].split(', ')
+            symbol_list = [gene["name"]] + gene["otheraliases"].split(", ")
+            if max >= 0:
+                symbol_list = symbol_list[:max]
+            symbols[gene_id] = symbol_list
 
     return symbols
 
-gene_symbols_for_id(["1738", "4508", "4540"])
 
 # %% First, let's generate a candidate set of variants that we'll want to parse.
 
@@ -41,54 +47,172 @@ PMCIDS = ["PMC6912785", "PMC6175136"]
 papers = [Paper(**{"id": pmcid, "is_pmc_oa": True, "pmcid": pmcid}) for pmcid in PMCIDS]
 # Use pubtator to get variants for these PMIDs.
 
-annotations = PubtatorEntityAnnotator().annotate(papers[0])
+# %% Collect a bunch of variant names and corresponding genes from papers, it's ok of these are noisy, with
+# incorrect gene associations (for example), since we're just going to use them as a smoke test
+# for HGVS parsing.
 
-# %%
+finder = re.compile(r"NM_[0-9]+\.[0-9]+")
 
-from lib.evagg.ref import NcbiGeneClient
-from lib.evagg.web.entrez import BioEntrezClient, BioEntrezDotEnvConfig
-gene_client = NcbiGeneClient(BioEntrezClient(BioEntrezDotEnvConfig()))
+# gene_client = NcbiGeneClient(BioEntrezClient(BioEntrezDotEnvConfig()))
 
-var_gene_ids = set()
-for p in annotations['passages']:
-    for a in p['annotations']:
-        if a['infons']['type'] == 'Mutation':
-            gene_id = a['infons'].get('gene_id', None)
-            if gene_id:
-                var_gene_ids.add(gene_id)
 
-gene_symbols = set()
-gene_identifiers = 
-for p in annotations['passages']:
-    for a in p['annotations']:
-        if a['infons']['type'] == 'Gene':
-            gene_symbols.add(a['text'])
-            print(a)
-            break
+def get_nearest_transcript(tx_matches: Sequence[Any], loc: int, max_dist: int) -> str | None:
+    closest_str = None
+    closest_distance = max_dist + 1
+    for match in tx_matches:
+        distance = min(abs(x - loc) for x in match.span())
+        if distance < closest_distance:
+            closest_str = match.group()
+            closest_distance = distance
+    return closest_str
 
-        
+def mane_select_for_protein(gene_symbol: str):
+    raise NotImplementedError()
 
-# %%
+variant_tuples = []
 
-gene_client.gene_id_for_symbol('Pt17')
+for idx, paper in enumerate(papers):
+    anno = PubtatorEntityAnnotator().annotate(paper)
 
-# %%
+    print(f"Analyzing paper {idx} - {paper}")
 
-for p in annotations['passages']:
-    for a in p['annotations']:
-        if a['infons']['type'] == 'Mutation':
-            if a['infons'].get('gene_id', 0) == 7634:
-                print(a)
+    # Preprocess the annotations to get a gene symbol lookup
+    gene_ids = set()
+    for p in anno["passages"]:
+        for a in p["annotations"]:
+            if a["infons"]["type"] == "Gene":
+                # sometimes they're semicolon delimited
+                gene_ids.update(a["infons"]["identifier"].split(";"))
+
+    gene_symbol_dict = gene_symbols_for_id(list(gene_ids), max=1)
+
+    for p in anno["passages"]:
+        tx_matches = [r for r in re.finditer(finder, p["text"])]
+        for a in p["annotations"]:
+            if a["infons"]["type"] == "Mutation":
+                v = {"mutation": a["text"]}
+
+                gene_id_int = a["infons"].get("gene_id", None)
+                if gene_id_int:
+                    matching_symbols = gene_symbol_dict.get(str(gene_id_int), None)
+                    if matching_symbols:
+                        v["gene"] = matching_symbols[0]
+                    else:
+                        v["gene"] = None
+                else:
+                    v["gene"] = None
+
+                if tx_matches:
+                    v["transcript"] = get_nearest_transcript(tx_matches, a["locations"][0]["offset"] - p["offset"], 100)
+                elif v["gene"]:
+                    v["transcript"] = None
+                else:
+                    v["transcript"] = None
+
+                variant_tuples.append(v)
+
+    break
+# %% Let's see how well LitVar's autocomplete does.
                 
-# %% Explore API
+from lib.evagg.ref.litvar import LitVarReference
 
-# create_variant()
-#  - pass in Gene and c.dot
-#            Gene and p.dot (but no transcript)
-#            Gene, p.dot, and transcript
-#            c.dot
-#            chr, pos, ref, alt
-#
-# ensure that the variant is biologically plausible (ie., the reference allele is actually in the reference)
+result = LitVarReference().variant_autocomplete("c.737C>T", limit=5)
+
+[r['gene'] for r in result]
+
+# Anecdotally, litvar isn't great at this. I've cherry picked an example here, but it's not a *rare* case
+# where litvar doesn't seem to pick up the correct gene for the variant. Interesting that pubtator seems to do better
+# even though they're using litvar under the hood for autocomplete. I think the proximity matching in the source text
+# is helping pubtator's performance.
+
+# %% Try to parse these variants.
+
+from hgvs.parser import Parser
+
+parser = Parser()
+
+v = parser.parse_hgvs_variant("NM_024996.7:c.689+908G>A")
+v.validate()
+
+# parse based on mutation and gene
+
+
+
+
 # %%
 
+import xml.etree.ElementTree as ET
+import time
+
+def check_mane_select(symbol: str) -> bool:
+    url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&retmode=json&term={symbol}[Gene] AND refseq_select[filter] AND "Homo sapiens"[Organism]'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    tx_entries = response.json()
+
+    if tx_entries['esearchresult']['count'] == '0':
+        print(f"Warning ({symbol}): MANE select transcript entries")
+        return False
+    elif tx_entries['esearchresult']['count'] != '1':
+        print(f"Warning ({symbol}): multiple MANE select entries ({tx_entries['esearchresult']['idlist']})")
+        return False
+    
+    tx_id = tx_entries['esearchresult']['idlist'][0]
+
+    # Get the whole nucleotide entry for this object ID
+    url2 = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=native&retmode=xml&id={tx_id}'
+    
+    response2 = requests.get(url2)
+    response2.raise_for_status()
+
+    tx_result = response2.text
+
+    time.sleep(1)
+
+    return tx_result.find("<GB-block_keywords_E>MANE Select</GB-block_keywords_E>") >= 0
+
+genes = set(d['gene'] for d in variant_tuples if d['gene'])
+
+for gene in list(genes):
+    if check_mane_select(gene):
+        print(f"MANE select transcript found for {gene}")
+
+# Here, on the original set of two papers, we see a couple interesting failure modes.
+# There are MANE select transcripts for some genes, but not all of them.
+# Warning (ERVK-1): MANE select transcript entries
+# Warning (ND5): MANE select transcript entries
+# Warning (ATP6): MANE select transcript entries
+# Warning (TAS2R6P): MANE select transcript entries
+
+# EVRK-1 is picked up as a synonym for CA1, which is actually mentioned in the second paper as a DOMAIN of interest in
+# PRKCG. This is a misassignment of associated gene by pubtator. Potentially we need to consider a better method for
+# gene association.
+
+
+
+# symbol="SRSF1"
+# url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&retmode=json&term={symbol}[Gene] AND refseq_select[filter] AND "Homo sapiens"[Organism]'
+# response = requests.get(url)
+# response.raise_for_status()
+
+# tx_entries = response.json()
+
+# if tx_entries['esearchresult']['count'] == '0':
+#     raise ValueError("No search results for gene symbol, do something different.")
+
+# tx_id = tx_entries['esearchresult']['idlist'][0]
+
+# url2 = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=native&retmode=xml&id={tx_id}'
+# # The below URL will return just the refseq ID for the transcript (which should be the human mane select if one exists)
+# # url2 = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=acc&retmode=text&id={tx_id}'
+# response2 = requests.get(url2)
+# response2.raise_for_status()
+
+# tx_result = response2.text
+
+# print(tx_result)
+
+# # https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&term=SRSF1[All%20Fields]%20AND%20refseq_select[filter]%20AND%20%22Homo%20sapiens%22[Organism]
+
+# %%
