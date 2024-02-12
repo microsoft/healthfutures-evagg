8c8290c9a414048e3307189dd5fabf170cd93f01
Greg Smith <gregsmi@microsoft.com>
2024-02-12 08:27:08 -0800

    updated log config structure

--- lib/evagg/svc/logging.py
+++ lib/evagg/svc/logging.py
@@ -2,6 +2,7 @@ import logging
 import logging.config
 import os
 from datetime import datetime
+import sys
 from typing import Any, Callable, Dict, List, Optional, Set
 
 PROMPT = logging.CRITICAL + 5
@@ -11,29 +12,19 @@ LogFilter = Callable[[logging.LogRecord], bool]
 LOGGING_CONFIG: Dict = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {
-        "color_log_formatter": {"()": "lib.evagg.svc.logging.ColorConsoleFormatter"},
-        "prompt_formatter": {"()": "lib.evagg.svc.logging.PromptFormatter"},
-    },
-    "filters": {
-        "console_filter": {"()": "lib.evagg.svc.logging.init_console_filter"},
-        "prompt_filter": {"()": "lib.evagg.svc.logging.init_prompt_filter"},
-    },
+    "filters": {"module_filter": {"()": "lib.evagg.svc.logging.init_module_filter"}},
     "handlers": {
-        "console_color": {
-            "class": "logging.StreamHandler",
-            "formatter": "color_log_formatter",
-            "stream": "ext://sys.stdout",
-            "filters": ["console_filter"],
+        "console_handler": {
+            "class": "lib.evagg.svc.logging.ConsoleHandler",
+            "filters": ["module_filter"],
         },
-        "prompt_files": {
-            "()": "lib.evagg.svc.logging.PromptHandler",
-            "formatter": "prompt_formatter",
-            "filters": ["prompt_filter"],
+        "prompt_handler": {
+            "()": "lib.evagg.svc.logging.PromptFileHandler",
+            "filters": ["module_filter"],
         },
     },
     "root": {
-        "handlers": ["console_color", "prompt_files"],
+        "handlers": ["console_handler", "prompt_handler"],
     },
 }
 
@@ -46,22 +37,11 @@ DEFAULT_EXCLUSIONS = [
 ]
 
 
-def init_prompt_filter(prompts_enabled: bool = False) -> LogFilter:
-    # Only handle prompt logs.
-    def filter(record: logging.LogRecord) -> bool:
-        return prompts_enabled and record.levelno == PROMPT
-
-    return filter
-
-
-def init_console_filter(exclude_modules: Set[str], include_modules: Set[str]) -> LogFilter:
+def init_module_filter(exclude_modules: Set[str], include_modules: Set[str]) -> LogFilter:
     def filter(record: logging.LogRecord) -> bool:
         def match_module(record: logging.LogRecord, module: str) -> bool:
             return record.name.startswith(module[:-1]) if module.endswith("*") else record.name == module
 
-        # Prompt logs are handled by the prompt handler.
-        if record.levelno == PROMPT:
-            return False
         # Don't filter out warnings and above.
         if record.levelno >= logging.WARNING:
             return True
@@ -72,7 +52,7 @@ def init_console_filter(exclude_modules: Set[str], include_modules: Set[str]) ->
     return filter
 
 
-class ColorConsoleFormatter(logging.Formatter):
+class LoggingFormatter(logging.Formatter):
     dim_grey = "\x1b[38;5;8m"
     grey = "\x1b[38;20m"
     yellow = "\x1b[38;5;11m"
@@ -89,97 +69,103 @@ class ColorConsoleFormatter(logging.Formatter):
         logging.CRITICAL: red + formatting + reset,
     }
 
-    def format(self, record: logging.LogRecord) -> str:
-        return self.format_console(record.levelno, record)
-
     @classmethod
-    def format_console(cls, formatno: int, record: logging.LogRecord) -> str:
+    def format_color(cls, formatno: int, record: logging.LogRecord) -> str:
         log_fmt = cls.FORMATS.get(formatno)
         formatter = logging.Formatter(log_fmt)
         # Strip "lib." prefix off of the record name.
         record.name = record.name.replace("lib.", "")
         return formatter.format(record)
 
-
-class PromptFormatter(logging.Formatter):
-    def format(self, record: logging.LogRecord) -> str:
-        return self.format_prompt(record)
-
     @classmethod
     def format_prompt(cls, record: logging.LogRecord) -> str:
         settings = record.__dict__.get("prompt_settings", {})
         prompt = record.__dict__.get("prompt_text", "")
-        result = record.__dict__.get("prompt_result", "")
+        response = record.__dict__.get("prompt_response", "")
         return (
-            f"#{' settings '.ljust(80, '#')}\n"
+            f"#{' SETTINGS '.ljust(80, '#')}\n"
             + f"{'\n'.join(f'{k}: {v}' for k, v in settings.items())}\n"
-            + f"#{' prompt '.ljust(80, '#')}\n"
+            + f"#{' PROMPT '.ljust(80, '#')}\n"
             + f"{prompt}\n"
-            + f"#{' result '.ljust(80, '#')}\n"
-            + f"{result}\n"
+            + f"#{' RESPONSE '.ljust(80, '#')}\n"
+            + f"{response}\n"
         )
 
 
-class PromptHandler(logging.Handler):
-    _log_dir: Optional[str] = None
-
-    def __init__(self, to_console: bool, to_file: bool, log_root: str, msg_level: Optional[int] = None) -> None:
-        self._to_file = to_file
-        self._to_console = to_console
-        self._msg_level = msg_level
+class PromptFileHandler(logging.Handler):
+    def __init__(self, log_root: Optional[str] = None) -> None:
         super().__init__()
 
-        if to_file:
+        if log_root is None:
+            self.addFilter(lambda _: False)
+        else:
+            self.addFilter(lambda record: record.levelno == PROMPT)
             self._log_dir = f"{log_root}/prompt_logs_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
             os.makedirs(self._log_dir, exist_ok=True)
 
     def emit(self, record: logging.LogRecord) -> None:
-        if 
-        with open(f"{self.log_dir}/{record.name}.log", "a") as f:
-            f.write(self.format(record) + "\n")
+        with open(f"{self._log_dir}/{record.name}.log", "a") as f:
+            f.write(LoggingFormatter.format_prompt(record) + "\n")
+
+
+class ConsoleHandler(logging.StreamHandler):
+    def __init__(self, prompts_enabled: bool, prompt_msgs_enabled: bool) -> None:
+        super().__init__(stream=sys.stdout)
+        self._prompts_enabled = prompts_enabled
+        self._prompt_msgs_enabled = prompt_msgs_enabled
+        # If prompts and prompt messages are both disabled, filter out prompt records.
+        if not prompts_enabled and not prompt_msgs_enabled:
+            self.addFilter(lambda record: record.levelno != PROMPT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        # Handle non-prompt records.
+        if record.levelno != PROMPT:
+            return LoggingFormatter.format_color(record.levelno, record)
+
+        outputs = []
+        if self._prompts_enabled:
+            outputs.append(LoggingFormatter.format_prompt(record))
+        if self._prompt_msgs_enabled:
+            outputs.append(LoggingFormatter.format_color(logging.INFO, record))
+        return "\n".join(outputs)
 
 
 class LogProvider:
     def __init__(
         self,
-        level: str = "WARNING",
+        level: Optional[str] = None,
         exclude_modules: Optional[List[str]] = None,
         include_modules: Optional[List[str]] = None,
-        exclude_defaults: bool = True,
-        prompts_to_file: bool = False,
-        prompts_to_console: bool = False,
-        prompt_output_root: str = ".out",
-        prompt_msg_level: str = "INFO",
+        exclude_defaults: Optional[bool] = True,
+        prompts_to_file: Optional[bool] = False,
+        prompts_to_console: Optional[bool] = False,
+        prompts_output_root: Optional[str] = ".out",
     ) -> None:
+        # Set up the base log level (defaults to WARNING).
+        level_number = self._level_to_number(level or "WARNING")
+        LOGGING_CONFIG["root"]["level"] = level_number
         # Add custom log level for prompts.
         logging.addLevelName(PROMPT, "PROMPT")
 
-        # Set up the base log level (defaults to WARNING).
-        level_number = self._level_to_number(level)
-        LOGGING_CONFIG["root"]["level"] = level_number
+        # Set up the prompt handler logging root if prompt files are enabled.
+        LOGGING_CONFIG["handlers"]["prompt_handler"]["log_root"] = prompts_output_root if prompts_to_file else None
 
-        # Set up the prompt handler's logging arguments.
-        LOGGING_CONFIG["handlers"]["prompt_files"]["to_console"] = prompts_to_console
-        LOGGING_CONFIG["handlers"]["prompt_files"]["to_file"] = prompts_to_file
-        LOGGING_CONFIG["handlers"]["prompt_files"]["log_root"] = prompt_output_root
-        # Allow prompt records through to the handler if either console or file logging is enabled.
-        LOGGING_CONFIG["filters"]["prompt_filter"]["prompts_enabled"] = prompts_to_console or prompts_to_file
+        # Set up the console handler logging arguments.
+        LOGGING_CONFIG["handlers"]["console_handler"]["prompts_enabled"] = prompts_to_console or False
+        LOGGING_CONFIG["handlers"]["console_handler"]["prompt_msgs_enabled"] = level_number >= logging.INFO
 
-        prompt_msg_level_number = self._level_to_number(prompt_msg_level)
-        if prompt_msg_level_number <= level_number:
-
-
-        # Set up the console filter's module logging arguments.
+        # Set up the module filter.
         exclusions = set(DEFAULT_EXCLUSIONS if exclude_defaults else [])
         exclusions.update(exclude_modules or [])
         inclusions = set(include_modules or [])
-        LOGGING_CONFIG["filters"]["console_filter"].update(
+        LOGGING_CONFIG["filters"]["module_filter"].update(
             {
                 "exclude_modules": exclusions,
                 "include_modules": inclusions,
             }
         )
 
+        # Set up the global logging configuration.
         logging.config.dictConfig(LOGGING_CONFIG)
 
         logger = logging.getLogger(__name__)
