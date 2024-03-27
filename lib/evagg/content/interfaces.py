27c3710ec502594549470374353b0bd1fba5f313
Miah Wander <miah@microsoft.com>
2024-03-27 16:37:17 +0000

    improve consolidation

--- lib/evagg/content/interfaces.py
+++ lib/evagg/content/interfaces.py
@@ -4,14 +4,18 @@ from lib.evagg.types import HGVSVariant, Paper
 
 
 class ICompareVariants(Protocol):
-    def consolidate(self, variants: Sequence[HGVSVariant]) -> Dict[HGVSVariant, Set[HGVSVariant]]:
+    def consolidate(
+        self, variants: Sequence[HGVSVariant], disregard_refseq: bool = False
+    ) -> Dict[HGVSVariant, Set[HGVSVariant]]:
         """Consolidate equivalent variants.
 
         Return a mapping from the retained variants to all variants collapsed into that variant.
         """
         ...  # pragma: no cover
 
-    def compare(self, variant1: HGVSVariant, variant2: HGVSVariant) -> HGVSVariant | None:
+    def compare(
+        self, variant1: HGVSVariant, variant2: HGVSVariant, disregard_refseq: bool = False
+    ) -> HGVSVariant | None:
         """Compare two variants to determine if they are biologically equivalent.
 
         If they are, return the more complete one, otherwise return None.
