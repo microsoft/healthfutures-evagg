cf87ae1abaac6c73037435315c9c102c9423fcd5
Greg Smith <gregsmi@microsoft.com>
2024-04-17 13:20:33 -0700

    async tests

--- test/evagg/test_llm.py
+++ test/evagg/test_llm.py
@@ -1,11 +1,11 @@
 from functools import reduce
-from unittest.mock import MagicMock, call, mock_open, patch
+from unittest.mock import AsyncMock, MagicMock, call, mock_open, patch
 
 from lib.evagg.llm import OpenAIClient
 
 
-@patch("lib.evagg.llm.aoai.AzureOpenAI")
-def test_openai_client_prompt(mock_openai, test_file_contents) -> None:
+@patch("lib.evagg.llm.aoai.AsyncAzureOpenAI", return_value=AsyncMock())
+async def test_openai_client_prompt(mock_openai, test_file_contents) -> None:
     prompt_template = test_file_contents("phenotype.txt")
     prompt_params = {"gene": "GENE", "variant": "VARIANT", "passage": "PASSAGE"}
     prompt_text = reduce(lambda x, kv: x.replace(f"{{{{${kv[0]}}}}}", kv[1]), prompt_params.items(), prompt_template)
@@ -13,7 +13,7 @@ def test_openai_client_prompt(mock_openai, test_file_contents) -> None:
     mock_openai.return_value.chat.completions.create.return_value.choices[0].message.content = "response"
     client = OpenAIClient({"deployment": "gpt-8", "endpoint": "https://ai", "api_key": "test", "api_version": "test"})
     with patch("builtins.open", mock_open(read_data=prompt_template)):
-        response = client.prompt_file(
+        response = await client.prompt_file(
             user_prompt_file="phenotype.txt",
             system_prompt="Extract field",
             params=prompt_params,
@@ -35,14 +35,14 @@ def test_openai_client_prompt(mock_openai, test_file_contents) -> None:
     )
 
 
-@patch("lib.evagg.llm.aoai.AzureOpenAI")
-def test_openai_client_embeddings(mock_openai) -> None:
+@patch("lib.evagg.llm.aoai.AsyncAzureOpenAI", return_value=AsyncMock())
+async def test_openai_client_embeddings(mock_openai) -> None:
     embedding = MagicMock(data=[MagicMock(embedding=[0.4, 0.5, 0.6])], usage=MagicMock(prompt_tokens=10))
     mock_openai.return_value.embeddings.create.return_value = embedding
 
     inputs = [f"input_{i}" for i in range(50)]
     client = OpenAIClient({"deployment": "gpt-8", "endpoint": "https://ai", "api_key": "test", "api_version": "test"})
-    response = client.embeddings(inputs)
+    response = await client.embeddings(inputs)
 
     mock_openai.assert_called_once_with(azure_endpoint="https://ai", api_key="test", api_version="test")
     mock_openai.return_value.embeddings.create.assert_has_calls(
