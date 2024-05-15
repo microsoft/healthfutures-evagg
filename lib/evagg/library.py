34dc98944750855b379491b29eeeb0d8644f0da4
ashleyatmsft <ashleyconard@microsoft.com>
2024-05-15 03:06:23 +0000

    added in negative examples

--- lib/evagg/library.py
+++ lib/evagg/library.py
@@ -253,6 +253,33 @@ class RareDiseaseFileLibrary(IGetPapers):
         return tables
 
     async def _get_llm_category(self, paper: Paper) -> str:
+        paper_finding_txt = "paper_finding.txt"
+        parameters = (
+            self._get_paper_texts(paper)
+            if paper_finding_txt == "paper_finding_full_text.txt"
+            else {
+                "abstract": paper.props.get("abstract") or "no abstract",
+                "title": paper.props.get("title") or "no title",
+            }
+        )
+        response = await self._llm_client.prompt_file(
+            user_prompt_file=os.path.join(os.path.dirname(__file__), "content", "prompts", paper_finding_txt),
+            system_prompt="Extract field",
+            params=parameters,
+            prompt_settings={"prompt_tag": "paper_category", "temperature": 0.8},
+        )
+
+        if isinstance(response, str):
+            result = response
+        else:
+            logger.warning(f"LLM failed to return a valid categorization response for {paper.id}: {response}")
+
+        if result in self.CATEGORIES:
+            return result
+
+        return "other"
+    
+    async def _get_llm_category_full_text(self, paper: Paper) -> str:
         paper_finding_txt = (
             "paper_finding.txt" if paper.props.get("full_text_xml") is None else "paper_finding_full_text.txt"
         )
@@ -320,7 +347,7 @@ class RareDiseaseFileLibrary(IGetPapers):
             f"Title: {paper.props.get('title') or 'no title'}\n"
             f"Abstract: {paper.props.get('abstract') or 'no abstract'}\n"
             
-            "Below are few shot examples of papers that are classified as rare disease:\n"
+            "Below are four few shot examples of papers that are classified as 'rare disease'. These are in no particular order:\n"
 
             "Gene: FBN2, PMID: 31316167\n"
             "Title: A clinical scoring system for congenital contractural arachnodactyly.\n"
@@ -337,6 +364,24 @@ class RareDiseaseFileLibrary(IGetPapers):
             "Gene: MLH3, PMID: 15193445\n"
             "Title: No association between two MLH3 variants (S845G and P844L)and colorectal cancer risk.\n"
             "Abstract: Recently we identified a new variant, S845G, in the MLH3 gene in 7 out of 327 patients suspected of hereditary nonpolyposis colorectal cancer but not fulfilling the Amsterdam criteria and in 1 out of 188 control subjects. As this variant might play a role in causing sporadic colorectal cancer, we analyzed its prevalence in sporadic colorectal cancer patients. We analyzed a small part of exon 1 of the MLH3 gene, including the S845G variant, in germline DNA of 467 white sporadic colorectal cancer patients and 497 white controls. The S845G variant was detected in five patients and eight controls; the results thus indicate that this variant does not confer an increased colorectal cancer risk. Another variant (P844L) was clearly a polymorphism. Three other missense variants were rare and the sample size of the study was too small to conclude whether they are pathogenic. In conclusion, no association was observed between two MLH3 variants (P844L and S845G) and colorectal cancer risk.\n"
+            
+            "Below are four few shot examples of papers that are classified as 'other'. These are in no particular order:\n"
+            
+            "Gene: MLH3, PMID: 20308424\n"
+            "Title: Mammalian BLM helicase is critical for integrating multiple pathways of meiotic recombination.\n"
+            "Abstract: Bloom's syndrome (BS) is an autosomal recessive disorder characterized by growth retardation, cancer predisposition, and sterility. BS mutated (Blm), the gene mutated in BS patients, is one of five mammalian RecQ helicases. Although BLM has been shown to promote genome stability by assisting in the repair of DNA structures that arise during homologous recombination in somatic cells, less is known about its role in meiotic recombination primarily because of the embryonic lethality associated with Blm deletion. However, the localization of BLM protein on meiotic chromosomes together with evidence from yeast and other organisms implicates a role for BLM helicase in meiotic recombination events, prompting us to explore the meiotic phenotype of mice bearing a conditional mutant allele of Blm. In this study, we show that BLM deficiency does not affect entry into prophase I but causes severe defects in meiotic progression. This is exemplified by improper pairing and synapsis of homologous chromosomes and altered processing of recombination intermediates, resulting in increased chiasmata. Our data provide the first analysis of BLM function in mammalian meiosis and strongly argue that BLM is involved in proper pairing, synapsis, and segregation of homologous chromosomes; however, it is dispensable for the accumulation of recombination intermediates.\n"
+
+            "Gene: ZNF423, PMID: 31234811\n"
+            "Title: Master regulator analysis of paragangliomas carrying SDHx, VHL, or MAML3 genetic alterations.\n"
+            "Abstract: Succinate dehydrogenase (SDH) loss and mastermind-like 3 (MAML3) translocation are two clinically important genetic alterations that correlate with increased rates of metastasis in subtypes of human paraganglioma and pheochromocytoma (PPGL) neuroendocrine tumors. Although hypotheses propose that succinate accumulation after SDH loss poisons dioxygenases and activates pseudohypoxia and epigenomic hypermethylation, it remains unclear whether these mechanisms account for oncogenic transcriptional patterns. Additionally, MAML3 translocation has recently been identified as a genetic alteration in PPGL, but is poorly understood. We hypothesize that a key to understanding tumorigenesis driven by these genetic alterations is identification of the transcription factors responsible for the observed oncogenic transcriptional changes.\n"
+
+            "Gene: RNASEH1, PMID: 35711919\n"
+            "Title: Case Report: Rare Homozygous RNASEH1 Mutations Associated With Adult-Onset Mitochondrial Encephalomyopathy and Multiple Mitochondrial DNA Deletions.\n"
+            "Abstract: Mitochondrial DNA (mtDNA) maintenance disorders embrace a broad range of clinical syndromes distinguished by the evidence of mtDNA depletion and/or deletions in affected tissues. Among the nuclear genes associated with mtDNA maintenance disorders, RNASEH1 mutations produce a homogeneous phenotype, with progressive external ophthalmoplegia (PEO), ptosis, limb weakness, cerebellar ataxia, and dysphagia. The encoded enzyme, ribonuclease H1, is involved in mtDNA replication, whose impairment leads to an increase in replication intermediates resulting from mtDNA replication slowdown. Here, we describe two unrelated Italian probands (Patient 1 and Patient 2) affected by chronic PEO, ptosis, and muscle weakness. Cerebellar features and severe dysphagia requiring enteral feeding were observed in one patient. In both cases, muscle biopsy revealed diffuse mitochondrial abnormalities and multiple mtDNA deletions. A targeted next-generation sequencing analysis revealed the homozygous RNASEH1 mutations c.129-3C>G and c.424G>A in patients 1 and 2, respectively. The c.129-3C>G substitution has never been described as disease-related and resulted in the loss of exon 2 in Patient 1 muscle RNASEH1 transcript. Overall, we recommend implementing the use of high-throughput sequencing approaches in the clinical setting to reach genetic diagnosis in case of suspected presentations with impaired mtDNA homeostasis.\n"
+
+            "Gene: TAPBP, PMID: 24159917\n"
+            "Title: Gastrointestinal stromal tumors: a case-only analysis of single nucleotide polymorphisms and somatic mutations.\n"
+            "Abstract: Gastrointestinal stromal tumors are rare soft tissue sarcomas that typically develop from mesenchymal cells with acquired gain-in-function mutations in KIT or PDGFRA oncogenes. These somatic mutations have been well-characterized, but little is known about inherited genetic risk factors. Given evidence that certain susceptibility loci and carcinogens are associated with characteristic mutations in other cancers, we hypothesized that these signature KIT or PDGFRA mutations may be similarly fundamental to understanding gastrointestinal stromal tumor etiology. Therefore, we examined associations between 522 single nucleotide polymorphisms and seven KIT or PDGFRA tumor mutations types. Candidate pathways included dioxin response, toxin metabolism, matrix metalloproteinase production, and immune and inflammatory response.\n"
         )
 
         process_response = str(process_response) + phrases
@@ -377,35 +422,6 @@ class RareDiseaseFileLibrary(IGetPapers):
 
         return "other"
 
-    def get_llm_category_w_few_shot(self, paper: Paper) -> str:
-        paper_finding_txt = (
-            "paper_finding.txt" if paper.props.get("full_text_xml") is None else "paper_finding_full_text.txt"
-        )
-        parameters = (
-            self._get_paper_texts(paper)
-            if paper_finding_txt == "paper_finding_full_text.txt"
-            else {
-                "abstract": paper.props.get("abstract") or "no abstract",
-                "title": paper.props.get("title") or "no title",
-            }
-        )
-        response = self._llm_client.prompt_file(
-            user_prompt_file=os.path.join(os.path.dirname(__file__), "content", "prompts", paper_finding_txt),
-            system_prompt="Extract field",
-            params=parameters,
-            prompt_settings={"prompt_tag": "paper_category", "temperature": 0.8},
-        )
-
-        if isinstance(response, str):
-            result = response
-        else:
-            logger.warning(f"LLM failed to return a valid categorization response for {paper.id}: {response}")
-
-        if result in self.CATEGORIES:
-            return result
-
-        return "other"
-
     async def _get_paper_categorizations(self, paper: Paper) -> str:
         """Categorize papers with multiple strategies and return the counts of each category."""
 
@@ -413,7 +429,7 @@ class RareDiseaseFileLibrary(IGetPapers):
         keyword_cat = self._get_keyword_category(paper)
         #print("keyword_cat", keyword_cat)
         # TODO: uncomment the line below to use the chain of thought approach
-        #llm_cat = await self._get_llm_category(paper)
+        # llm_cat = await self._get_llm_category(paper)
         llm_cat = await self._apply_chain_of_thought(paper)
         #print("llm_cat", llm_cat)
         # llm_cat = self._collect_neg_pos_papers(paper, all_papers)
