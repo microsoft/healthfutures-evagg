094737a975e0c9cac7efa7ca213cba49e75f9f69
Miah Wander <miah@microsoft.com>
2024-05-16 19:21:45 +0000

    full text as section objects

--- lib/evagg/content/fulltext.py
+++ lib/evagg/content/fulltext.py
@@ -1,22 +1,25 @@
-from dataclasses import dataclass
 from typing import Generator, List, Optional
 from xml.etree.ElementTree import Element
+
+from .interfaces import TextSection
 
 SectionFilter = Optional[List[str]]
 
 
-@dataclass(frozen=True)
-class TextSection:
-    section_type: str
-    text_type: str
-    offset: int
-    text: str
-
-
-def get_fulltext_sections(doc: Optional[Element]) -> Generator[TextSection, None, None]:
+def get_sections(
+    doc: Optional[Element], include: SectionFilter = None, exclude: SectionFilter = None
+) -> Generator[TextSection, None, None]:
     """Build a list of TextSection objects from the XML full-text document."""
     if doc is None:
         return
+
+    def _include_section(section: TextSection) -> bool:
+        if include and section.section_type not in include:
+            return False
+        if exclude and section.section_type in exclude:
+            return False
+        return True
+
     doc_id = doc.findtext("id") or "unknown"
     # Generate all "passage" elements.
     for passage in doc.iterfind("./passage"):
@@ -26,26 +29,24 @@ def get_fulltext_sections(doc: Optional[Element]) -> Generator[TextSection, None
             raise ValueError(f"Missing 'type' infon element in passage for document {doc_id}")
         if not (offset := passage.findtext("offset")):
             raise ValueError(f"Missing 'offset' infon element in {section_type} passage for document {doc_id}")
-        # Eliminate linebreaks and strip leading/trailing whitespace from each line.
         text = " ".join(s.strip() for s in (passage.findtext("text") or "").split("\n"))
-        yield TextSection(section_type, text_type, int(offset), text)
+        section = TextSection(section_type, text_type, int(offset), text)
+        if _include_section(section):
+            # Eliminate linebreaks and strip leading/trailing whitespace from each line.
+            yield section
 
 
 def get_section_texts(
     doc: Optional[Element], include: SectionFilter = None, exclude: SectionFilter = None
 ) -> Generator[str, None, None]:
     """Filter to the given sections in the XML full-text document and return the texts."""
+    for section in get_sections(doc, include, exclude):
+        yield section.text
 
-    def _include_section(section: TextSection) -> bool:
-        if include and section.section_type not in include:
-            return False
-        if exclude and section.section_type in exclude:
-            return False
-        return True
 
-    for section in get_fulltext_sections(doc):
-        if _include_section(section):
-            yield section.text
+def get_all_sections(doc: Optional[Element]) -> Generator[TextSection, None, None]:
+    """Extract all sections from the XML full-text document."""
+    return get_sections(doc)
 
 
 def get_fulltext(doc: Optional[Element], include: SectionFilter = None, exclude: SectionFilter = None) -> str:
