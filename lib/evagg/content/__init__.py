591945b991e9f65e01dec0c13cecc07139929ed1
Greg Smith <gregsmi@microsoft.com>
2024-05-22 09:59:03 -0700

    moved simple extractor

--- lib/evagg/content/__init__.py
+++ lib/evagg/content/__init__.py
@@ -3,14 +3,12 @@
 from .interfaces import IFindObservations, Observation, TextSection
 from .observation import ObservationFinder, TruthsetObservationFinder
 from .prompt_based import PromptBasedContentExtractor
-from .simple import SimpleContentExtractor
 from .truth_set import TruthsetContentExtractor
 from .variant import HGVSVariantComparator, HGVSVariantFactory
 
 __all__ = [
     # Content
     "PromptBasedContentExtractor",
-    "SimpleContentExtractor",
     "TruthsetContentExtractor",
     # Interfaces.
     "IFindObservations",
