66ad6cac3d888174831f5f50b167fbeea6832fe4
Greg Smith <gregsmi@microsoft.com>
2024-05-22 09:54:37 -0700

    created simple.py

---
+++ lib/evagg/simple.py
@@ -0,0 +1,35 @@
+import json
+import os
+from typing import Any, Dict, Sequence
+
+from lib.evagg.types import Paper
+
+from .interfaces import IGetPapers
+
+
+class SimpleFileLibrary(IGetPapers):
+    def __init__(self, collections: Sequence[str]) -> None:
+        self._collections = collections
+
+    def _load_collection(self, collection: str) -> Dict[str, Paper]:
+        papers = {}
+        # collection is a local directory, get a list of all of the json files in that directory
+        for filename in os.listdir(collection):
+            if filename.endswith(".json"):
+                # load the json file into a dict and append it to papers
+                with open(os.path.join(collection, filename), "r") as f:
+                    paper = Paper(**json.load(f))
+                    papers[paper.id] = paper
+        return papers
+
+    def _load(self) -> Dict[str, Paper]:
+        papers = {}
+        for collection in self._collections:
+            papers.update(self._load_collection(collection))
+
+        return papers
+
+    def get_papers(self, query: Dict[str, Any]) -> Sequence[Paper]:
+        # Dummy implementation that returns all papers regardless of query.
+        all_papers = list(self._load().values())
+        return all_papers
