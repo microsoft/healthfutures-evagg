5b7cfc82bd1cd8993b96673328ecd52e4a974fd6
Greg Smith <gregsmi@microsoft.com>
2024-05-22 09:54:37 -0700

    created simple.py

--- lib/evagg/library.py
+++ lib/evagg/library.py
@@ -1,6 +1,5 @@
 import asyncio
 import csv
-import json
 import logging
 import os
 import re
@@ -18,37 +17,6 @@ from .disease_keywords import INCLUSION_KEYWORDS
 from .interfaces import IGetPapers
 
 logger = logging.getLogger(__name__)
-# TODO: ways to improve:
-#       - process full text of paper to filter to rare disease papers when PMC OA
-
-
-class SimpleFileLibrary(IGetPapers):
-    def __init__(self, collections: Sequence[str]) -> None:
-        self._collections = collections
-
-    def _load_collection(self, collection: str) -> Dict[str, Paper]:
-        papers = {}
-        # collection is a local directory, get a list of all of the json files in that directory
-        for filename in os.listdir(collection):
-            if filename.endswith(".json"):
-                # load the json file into a dict and append it to papers
-                with open(os.path.join(collection, filename), "r") as f:
-                    paper = Paper(**json.load(f))
-                    papers[paper.id] = paper
-        return papers
-
-    @cache
-    def _load(self) -> Dict[str, Paper]:
-        papers = {}
-        for collection in self._collections:
-            papers.update(self._load_collection(collection))
-
-        return papers
-
-    def get_papers(self, query: Dict[str, Any]) -> Sequence[Paper]:
-        # Dummy implementation that returns all papers regardless of query.
-        all_papers = list(self._load().values())
-        return all_papers
 
 
 # These are the columns in the truthset that are specific to the paper.
