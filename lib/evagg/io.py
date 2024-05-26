0f60fae7305a02bee054222145bc8191646d8d1c
Greg Smith <gregsmi@microsoft.com>
2024-05-26 09:50:42 -0700

    added run.py

--- lib/evagg/io.py
+++ lib/evagg/io.py
@@ -5,7 +5,7 @@ import sys
 from datetime import datetime
 from typing import Mapping, Optional, Sequence
 
-from lib.evagg.utils.logging import get_run_root
+from lib.evagg.utils.run import get_run_path
 
 from .interfaces import IWriteOutput
 
@@ -15,14 +15,16 @@ logger = logging.getLogger(__name__)
 class TableOutputWriter(IWriteOutput):
     def __init__(self, tsv_name: Optional[str] = None) -> None:
         self._generated = datetime.now().astimezone()
-        self._path = os.path.join(get_run_root(), f"{tsv_name}.tsv") if tsv_name else None
+        self._path = os.path.join(get_run_path(), f"{tsv_name}.tsv") if tsv_name else None
+        if self._path and os.path.exists(self._path):
+            logger.warning(f"Overwriting existing output file: {self._path}")
 
-    def write(self, output: Sequence[Mapping[str, str]]) -> None:
+    def write(self, output: Sequence[Mapping[str, str]]) -> Optional[str]:
         logger.info(f"Writing output to: {self._path or 'stdout'}")
 
         if len(output) == 0:
             logger.warning("No results to write")
-            return
+            return None
 
         if self._path:
             parent = os.path.dirname(self._path)
@@ -36,9 +38,10 @@ class TableOutputWriter(IWriteOutput):
         field_names = output[0].keys()
         writer.writerow(field_names)
         for line in output:
-            # For table output, all rows must have the same keys.
+            # For table output, all rows should have the same keys.
             assert line.keys() == field_names
             writer.writerow(line.values())
 
         if self._path:
             output_stream.close()
+        return self._path
