d62a5055590bab25e93bb1424f3907b0ba13c593
Greg Smith <gregsmi@microsoft.com>
2024-06-07 11:44:44 -0700

    rationalization of output columns

--- test/evagg/types/test_base.py
+++ test/evagg/types/test_base.py
@@ -29,8 +29,8 @@ def test_paper_from_dict() -> None:
     paper_dict = {"id": "123", "citation": "Test Citation", "abstract": "Test Abstract", "pmcid": "PMC123"}
     paper = Paper(**paper_dict)
     assert paper.id == "123"
-    assert paper.citation == "Test Citation"
-    assert paper.abstract == "Test Abstract"
+    assert paper.props["citation"] == "Test Citation"
+    assert paper.props["abstract"] == "Test Abstract"
     assert paper.props["pmcid"] == "PMC123"
 
 
