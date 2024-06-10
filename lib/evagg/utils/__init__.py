1f1844623926c9b7745cf7486ee61bcf99982af1
Greg Smith <gregsmi@microsoft.com>
2024-06-10 14:10:54 -0700

    added include_negative_examples

--- lib/evagg/utils/__init__.py
+++ lib/evagg/utils/__init__.py
@@ -1,15 +1,10 @@
 """Package for utilities."""
 
-import os
-
 from .logging import init_logger
 from .settings import get_azure_credential, get_dotenv_settings, get_env_settings
 from .web import CosmosCachingWebClient, IWebContentClient, RequestsWebContentClient
 
-PROMPT_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "content", "prompts")
-
 __all__ = [
-    "PROMPT_DIR",
     # Settings.
     "get_azure_credential",
     "get_dotenv_settings",
