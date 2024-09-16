79cca41e399f0ef015bf8f3486962f9bf04dba47
Miah Wander <miah@microsoft.com>
2024-09-16 19:20:27 +0000

    paper finding figures, make both train and test

--- scripts/benchmark/paper_finding/manuscript_generate_figures.py
+++ scripts/benchmark/paper_finding/manuscript_generate_figures.py
@@ -7,6 +7,7 @@ from typing import Any, Dict, List
 
 import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
 
 # %% Constants.
 
@@ -42,102 +43,113 @@ def load_run(run_id: str) -> pd.DataFrame | None:
     return run_data
 
 
-# %% Generate stats for each run.
+# %% Generate sta
 
-train_runs = [load_run(id) for id in TEST_RUNS]
+all_run_stats: Dict[str, pd.DataFrame] = {}
 
-train_run_stats_dicts: List[Dict[str, Any]] = []
+for run_type, run_ids in [("train", TRAIN_RUNS), ("test", TEST_RUNS)]:
 
-for run_id, run in zip(TEST_RUNS, train_runs):
-    if run is None:
-        continue
+    runs = [load_run(id) for id in run_ids]
 
-    n_correct = run.query("in_truth == True and in_pipeline == True").shape[0]
-    missed = run.query("in_truth == True and in_pipeline == False")
-    n_missed = missed.shape[0]
-    missed_queries = set(missed["pmid_with_query"].unique())
-    irrelevant = run.query("in_truth == False and in_pipeline == True")
-    n_irrelevant = irrelevant.shape[0]
-    irrelevant_queries = set(irrelevant["pmid_with_query"].unique())
+    run_stats_dicts: List[Dict[str, Any]] = []
 
-    train_run_stats_dicts.append(
-        {
-            "run_id": run_id,
-            "n_correct": n_correct,
-            "n_missed": n_missed,
-            "n_irrelevant": n_irrelevant,
-            "missed": missed_queries,
-            "irrelevant": irrelevant_queries,
-        }
-    )
+    for run_id, run in zip(run_ids, runs):
+        if run is None:
+            continue
 
-# Make a dataframe out of run_counts_dicts.
-train_run_stats = pd.DataFrame(train_run_stats_dicts)
+        n_correct = run.query("in_truth == True and in_pipeline == True").shape[0]
+        missed = run.query("in_truth == True and in_pipeline == False")
+        n_missed = missed.shape[0]
+        missed_queries = set(missed["pmid_with_query"].unique())
+        irrelevant = run.query("in_truth == False and in_pipeline == True")
+        n_irrelevant = irrelevant.shape[0]
+        irrelevant_queries = set(irrelevant["pmid_with_query"].unique())
 
-# Add derived stats.
-train_run_stats["precision"] = train_run_stats["n_correct"] / (
-    train_run_stats["n_correct"] + train_run_stats["n_irrelevant"]
-)
-train_run_stats["recall"] = train_run_stats["n_correct"] / (train_run_stats["n_correct"] + train_run_stats["n_missed"])
-train_run_stats["f1"] = (
-    2
-    * train_run_stats["precision"]
-    * train_run_stats["recall"]
-    / (train_run_stats["precision"] + train_run_stats["recall"])
-)
+        run_stats_dicts.append(
+            {
+                "run_id": run_id,
+                "n_correct": n_correct,
+                "n_missed": n_missed,
+                "n_irrelevant": n_irrelevant,
+                "missed": missed_queries,
+                "irrelevant": irrelevant_queries,
+            }
+        )
 
-# Reorder columns, putting missed and irrelevant at the end.
-train_run_stats = train_run_stats[
-    list(train_run_stats.columns.drop(["missed", "irrelevant"])) + ["missed", "irrelevant"]
-]
+    # Make a dataframe out of run_counts_dicts.
+    run_stats = pd.DataFrame(run_stats_dicts)
+
+    # Add derived stats.
+    run_stats["precision"] = run_stats["n_correct"] / (run_stats["n_correct"] + run_stats["n_irrelevant"])
+    run_stats["recall"] = run_stats["n_correct"] / (run_stats["n_correct"] + run_stats["n_missed"])
+    run_stats["f1"] = 2 * run_stats["precision"] * run_stats["recall"] / (run_stats["precision"] + run_stats["recall"])
+
+    # Reorder columns, putting missed and irrelevant at the end.
+    run_stats = run_stats[list(run_stats.columns.drop(["missed", "irrelevant"])) + ["missed", "irrelevant"]]
+
+    all_run_stats[run_type] = run_stats
 
 # %% Make the counts performance barplot.
 sns.set_theme(style="whitegrid")
 
-# Convert this dataframe to have the columns: run_id, count_type, and count_value.
-run_counts_melted = train_run_stats[["run_id", "n_correct", "n_missed", "n_irrelevant"]].melt(
-    id_vars="run_id", var_name="count_type", value_name="count_value"
-)
+for run_type in ["train", "test"]:
+    run_stats = all_run_stats[run_type]
 
-g = sns.barplot(
-    data=run_counts_melted,
-    x="count_type",
-    y="count_value",
-    errorbar="sd",
-    alpha=0.6,
-)
-g.xaxis.set_label_text("")
-g.yaxis.set_label_text("Papers in category")
-g.title.set_text("Paper finding benchmark results (train)")
+    # Convert this dataframe to have the columns: run_id, count_type, and count_value.
+    run_counts_melted = run_stats[["run_id", "n_correct", "n_missed", "n_irrelevant"]].melt(
+        id_vars="run_id", var_name="count_type", value_name="count_value"
+    )
+
+    plt.figure()
+
+    g = sns.barplot(
+        data=run_counts_melted,
+        x="count_type",
+        y="count_value",
+        errorbar="sd",
+        alpha=0.6,
+    )
+    g.xaxis.set_label_text("")
+    g.yaxis.set_label_text("Papers in category")
+    g.title.set_text(f"Paper finding benchmark results ({run_type}; N={run_stats.shape[0]})")
 
 # %% Make the stats performance barplot.
 sns.set_theme(style="whitegrid")
 
-# Convert this dataframe to have the columns: run_id, count_type, and count_value.
-run_stats_melted = train_run_stats[["run_id", "precision", "recall", "f1"]].melt(
-    id_vars="run_id", var_name="stat_type", value_name="stat_value"
-)
+for run_type in ["train", "test"]:
+    run_stats = all_run_stats[run_type]
 
-g = sns.barplot(
-    data=run_stats_melted,
-    x="stat_type",
-    y="stat_value",
-    errorbar="sd",
-    alpha=0.6,
-)
+    # Convert this dataframe to have the columns: run_id, count_type, and count_value.
+    run_stats_melted = run_stats[["run_id", "precision", "recall", "f1"]].melt(
+        id_vars="run_id", var_name="stat_type", value_name="stat_value"
+    )
 
-g.xaxis.set_label_text("")
-g.yaxis.set_label_text("Performance metric")
-g.title.set_text("Paper finding benchmark results (train) - dumb way to display")
+    plt.figure()
+
+    g = sns.barplot(
+        data=run_stats_melted,
+        x="stat_type",
+        y="stat_value",
+        errorbar="sd",
+        alpha=0.6,
+    )
+
+    g.xaxis.set_label_text("")
+    g.yaxis.set_label_text("Performance metric")
+    g.title.set_text(f"Paper finding benchmark results ({run_type}; N={run_stats.shape[0]}) - dumb way to display")
 
 # %% Print them instead.
 
-print(train_run_stats[["n_correct", "n_missed", "n_irrelevant", "precision", "recall", "f1"]])
+for run_type in ["train", "test"]:
+    run_stats = all_run_stats[run_type]
 
-print()
-print(
-    train_run_stats[["n_correct", "n_missed", "n_irrelevant", "precision", "recall", "f1"]].aggregate(["mean", "std"])
-)
+    print(f"-- Paper finding benchmark results ({run_type}; N={run_stats.shape[0]}) --")
+
+    print(run_stats[["n_correct", "n_missed", "n_irrelevant", "precision", "recall", "f1"]])
+
+    print()
+    print(run_stats[["n_correct", "n_missed", "n_irrelevant", "precision", "recall", "f1"]].aggregate(["mean", "std"]))
+    print()
 
 
 # %%
