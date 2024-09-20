ba514f6c58881aa54949bbe94692890fcca6e9d5
Miah Wander <miah@microsoft.com>
2024-09-20 05:49:15 +0000

    content extraction scripts

---
+++ scripts/benchmark/content_extraction/manuscript_generate_content_figures.py
@@ -0,0 +1,172 @@
+"""This notebook is intended to be used to generate paper finding benchmark figures for the manuscript."""
+
+# %% Imports.
+
+import os
+from typing import Any, Dict, List
+
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# %% Constants.
+
+TRAIN_RUNS = [
+    "20240909_165847",
+    "20240909_210652",
+    "20240910_044027",
+    "20240910_134659",
+    "20240910_191020",
+]
+
+TEST_RUNS = [
+    "20240911_165451",
+    "20240911_194240",
+    "20240911_223218",
+    "20240912_145606",
+    "20240912_181121",
+]
+
+CONTENT_COLUMNS = [
+    "animal_model",
+    "engineered_cells",
+    "patient_cells_tissues",
+    "phenotype",
+    "study_type",
+    "variant_inheritance",
+    "variant_type",
+    "zygosity",
+]
+
+# %% Function definitions.
+
+
+def load_run(run_id: str, run_filename: str) -> pd.DataFrame | None:
+    """Load the data from a single run."""
+    run_file = f".out/run_evagg_pipeline_{run_id}_content_extraction_benchmarks/{run_filename}"
+    if not os.path.exists(run_file):
+        print(
+            f"No benchmark analysis exists for run_id {run_file}. "
+            "Do you need to run 'manuscript_content_extraction.py' first?"
+        )
+        return None
+
+    run_data = pd.read_csv(run_file, sep="\t")
+    return run_data
+
+
+def get_eval_df(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    if df.empty:
+        return df
+
+    assert df[f"{column}_result_type"].nunique() == 1
+
+    result_type = df[f"{column}_result_type"].iloc[0]
+
+    if result_type == "I":
+        eval_df = df[~df.reset_index().set_index(["paper_id", "individual_id"]).index.duplicated(keep="first")]
+    elif result_type == "IV":
+        eval_df = df[
+            ~df.reset_index().set_index(["paper_id", "hgvs_desc", "individual_id"]).index.duplicated(keep="first")
+        ]
+    elif result_type == "P":
+        eval_df = df[~df.reset_index().set_index(["paper_id"]).index.duplicated(keep="first")]
+    elif result_type == "V":
+        eval_df = df[~df.reset_index().set_index(["paper_id", "hgvs_desc"]).index.duplicated(keep="first")]
+    else:
+        raise ValueError(f"Unknown result type: {result_type}")
+
+    return eval_df[[f"{column}_result", f"{column}_truth", f"{column}_output"]]
+
+
+# %% Generate run stats for content extraction.
+
+all_obs_run_stats: Dict[str, pd.DataFrame] = {}
+
+for run_type, run_ids in [("train", TRAIN_RUNS), ("test", TEST_RUNS)]:
+
+    runs = [load_run(id, "content_extraction_results.tsv") for id in run_ids]
+
+    obs_run_stats_dicts: List[Dict[str, Any]] = []
+
+    for run_id, run in zip(run_ids, runs):
+        if run is None:
+            continue
+
+        run_dict: Dict[str, Any] = {
+            "run_id": run_id,
+        }
+
+        for column in CONTENT_COLUMNS:
+
+            eval_df = get_eval_df(run, column)
+
+            if column == "phenotype":
+                phenotype_lists = eval_df[f"{column}_result"].apply(lambda x: x.split("["))
+                assert phenotype_lists.apply(lambda x: len(x) == 5).all()
+                result = phenotype_lists.apply(lambda x: ((len(x[3]) < 10) & (len(x[4]) < 10))).mean()
+            else:
+                result = eval_df[f"{column}_result"].mean()
+
+            run_dict[f"{column}_n"] = eval_df.shape[0]
+            run_dict[column] = result
+
+        obs_run_stats_dicts.append(run_dict)
+
+    run_stats = pd.DataFrame(obs_run_stats_dicts)
+
+    all_obs_run_stats[run_type] = run_stats
+
+
+# %% Make the performance barplot.
+sns.set_theme(style="whitegrid")
+
+for run_type in ["train", "test"]:
+    run_stats = all_obs_run_stats[run_type]
+
+    content_perf_melted = run_stats[["run_id"] + CONTENT_COLUMNS].melt(
+        id_vars="run_id", var_name="field", value_name="accuracy"
+    )
+
+    plt.figure()
+
+    g = sns.barplot(
+        data=content_perf_melted,
+        x="field",
+        y="accuracy",
+        errorbar="sd",
+        alpha=0.6,
+    )
+    g.xaxis.set_label_text("")
+    g.yaxis.set_label_text("Accuracy")
+    g.title.set_text(f"Content extraction benchmark results ({run_type}; N={run_stats.shape[0]})")
+
+    # rotate the x-axis labels
+    for item in g.get_xticklabels():
+        item.set_rotation(90)
+
+
+# %% Print them instead.
+
+max_columns = pd.get_option("display.max_columns")
+pd.set_option("display.max_columns", None)
+
+width = pd.get_option("display.width")
+pd.set_option("display.width", 2000)
+
+for run_type in ["train", "test"]:
+    run_stats = all_obs_run_stats[run_type]
+
+    print(f"-- Content extraction benchmark results ({run_type}; N={run_stats.shape[0]}) --")
+
+    print(run_stats[["run_id"] + CONTENT_COLUMNS])
+
+    print()
+    print(run_stats[CONTENT_COLUMNS].aggregate(["mean", "std"]))
+    print()
+
+pd.set_option("display.max_columns", max_columns)
+pd.set_option("display.width", width)
+
+
+# %%
---
+++ scripts/benchmark/content_extraction/manuscript_generate_obs_figures.py
@@ -0,0 +1,168 @@
+"""This notebook is intended to be used to generate paper finding benchmark figures for the manuscript."""
+
+# %% Imports.
+
+import os
+from typing import Any, Dict, List
+
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# %% Constants.
+
+TRAIN_RUNS = [
+    "20240909_165847",
+    "20240909_210652",
+    "20240910_044027",
+    "20240910_134659",
+    "20240910_191020",
+]
+
+TEST_RUNS = [
+    "20240911_165451",
+    "20240911_194240",
+    "20240911_223218",
+    "20240912_145606",
+    "20240912_181121",
+]
+
+# %% Function definitions.
+
+
+def load_run(run_id: str, run_filename: str) -> pd.DataFrame | None:
+    """Load the data from a single run."""
+    run_file = f".out/run_evagg_pipeline_{run_id}_content_extraction_benchmarks/{run_filename}"
+    if not os.path.exists(run_file):
+        print(
+            f"No benchmark analysis exists for run_id {run_file}. "
+            "Do you need to run 'manuscript_content_extraction.py' first?"
+        )
+        return None
+
+    run_data = pd.read_csv(run_file, sep="\t")
+    return run_data
+
+
+# %% Generate run stats for observation finding.
+
+all_obs_run_stats: Dict[str, pd.DataFrame] = {}
+
+for run_type, run_ids in [("train", TRAIN_RUNS), ("test", TEST_RUNS)]:
+
+    runs = [load_run(id, "observation_finding_results.tsv") for id in run_ids]
+
+    obs_run_stats_dicts: List[Dict[str, Any]] = []
+
+    for run_id, run in zip(run_ids, runs):
+        if run is None:
+            continue
+
+        precision = run.in_truth[run.in_pipeline].mean()
+        recall = run.in_pipeline[run.in_truth].mean()
+        n = run.shape[0]
+
+        # Count the variant as being present in truth or pipeline output if it's present in any row, regardless of the
+        # individual_id for that row.
+        for _, grp_df in run.groupby(["paper_id", "hgvs_desc"]):
+            if grp_df.in_truth.any():
+                run.loc[grp_df.index, "in_truth"] = True
+            if grp_df.in_pipeline.any():
+                run.loc[grp_df.index, "in_pipeline"] = True
+
+        run.drop_duplicates(subset=["paper_id", "hgvs_desc"], inplace=True, keep="first")
+
+        precision_variant = run.in_truth[run.in_pipeline].mean()
+        recall_variant = run.in_pipeline[run.in_truth].mean()
+        n_variant = run.shape[0]
+
+        obs_run_stats_dicts.append(
+            {
+                "run_id": run_id,
+                "n": n,
+                "precision": precision,
+                "recall": recall,
+                "f1": 2 * precision * recall / (precision + recall),
+                "n_variant": n_variant,
+                "precision_variant": precision_variant,
+                "recall_variant": recall_variant,
+                "f1_variant": 2 * precision_variant * recall_variant / (precision_variant + recall_variant),
+            }
+        )
+
+    run_stats = pd.DataFrame(obs_run_stats_dicts)
+
+    all_obs_run_stats[run_type] = run_stats
+
+# %% Make the performance barplot.
+sns.set_theme(style="whitegrid")
+
+for run_type in ["train", "test"]:
+    run_stats = all_obs_run_stats[run_type]
+
+    obs_perf_melted = run_stats[["run_id", "precision", "recall"]].melt(
+        id_vars="run_id", var_name="metric", value_name="result"
+    )
+
+    plt.figure()
+
+    g = sns.barplot(
+        data=obs_perf_melted,
+        x="metric",
+        y="result",
+        errorbar="sd",
+        alpha=0.6,
+    )
+    g.xaxis.set_label_text("")
+    g.yaxis.set_label_text("Performance")
+    g.title.set_text(f"Observation finding benchmark results ({run_type}; N={run_stats.shape[0]})")
+
+    var_perf_melted = run_stats[["run_id", "precision_variant", "recall_variant"]].melt(
+        id_vars="run_id", var_name="metric", value_name="result"
+    )
+    var_perf_melted["metric"] = var_perf_melted["metric"].map(
+        {"precision_variant": "precision", "recall_variant": "recall"}
+    )
+
+    plt.figure()
+
+    g = sns.barplot(
+        data=var_perf_melted,
+        x="metric",
+        y="result",
+        errorbar="sd",
+        alpha=0.6,
+    )
+    g.xaxis.set_label_text("")
+    g.yaxis.set_label_text("Performance")
+    g.title.set_text(f"Variant finding benchmark results ({run_type}; N={run_stats.shape[0]})")
+
+
+# %% Print them instead.
+
+max_columns = pd.get_option("display.max_columns")
+pd.set_option("display.max_columns", None)
+
+width = pd.get_option("display.width")
+pd.set_option("display.width", 2000)
+
+for run_type in ["train", "test"]:
+    run_stats = all_obs_run_stats[run_type]
+
+    print(f"-- Paper finding benchmark results ({run_type}; N={run_stats.shape[0]}) --")
+
+    print(run_stats[["n", "precision", "recall", "n_variant", "precision_variant", "recall_variant"]])
+
+    print()
+    print(
+        run_stats[["n", "precision", "recall", "n_variant", "precision_variant", "recall_variant"]].aggregate(
+            ["mean", "std"]
+        )
+    )
+    print()
+
+pd.set_option("display.max_columns", max_columns)
+pd.set_option("display.width", width)
+
+
+# %%
