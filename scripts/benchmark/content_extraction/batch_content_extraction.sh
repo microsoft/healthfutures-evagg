24294852aa5905beaa8cd6b4cccff0e3294e1d9a
Miah Wander <miah@microsoft.com>
2024-09-30 19:46:05 +0000

    rename duplicated modules to make mypy happy

--- scripts/benchmark/content_extraction/batch_content_extraction.sh
+++ scripts/benchmark/content_extraction/batch_content_extraction.sh
@@ -6,12 +6,12 @@ TEST=(165451 194240 223218 145606 181121)
 
 # Loop through each element in the TRAIN list
 for run_id in "${TRAIN[@]}"; do
-    python scripts/benchmark/content_extraction/manuscript_single_run.py \
+    python scripts/benchmark/content_extraction/manuscript_obs_content_single_run.py \
     -m data/v1/evidence_train_v1.tsv -p "$run_id"
 done
 
 # Loop through each element in the TEST list
 for run_id in "${TEST[@]}"; do
-    python scripts/benchmark/content_extraction/manuscript_single_run.py \
+    python scripts/benchmark/content_extraction/manuscript_obs_content_single_run.py \
     -m data/v1/evidence_test_v1.tsv -p "$run_id"
 done
