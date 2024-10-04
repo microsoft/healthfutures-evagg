44056229365ef9ff705721de8b9235c494abb1b9
Miah Wander <miah@microsoft.com>
2024-10-04 17:00:51 +0000

    changes to question generation

--- scripts/benchmark/generate_discrepancy_questions.py
+++ scripts/benchmark/generate_discrepancy_questions.py
@@ -1,4 +1,4 @@
-"""This notebook is used to consolidate discrepancy questions into a single text file.
+"""This notebook is used to generate text questions for the discrepancy resolution task.
 
 This notebook can only be run after paper_finding/manuscript_discrepancy_list.py,
 content_extraction/manuscript_obs_discrepancy_list.py, and content_extraction/manuscript_content_discrepancy_list.py
@@ -27,6 +27,8 @@ OBSERVATION_DISCREPANCY_FILE = ".out/manuscript_content_extraction/all_observati
 
 CONTENT_DISCREPANCY_ROOT = ".out/manuscript_content_extraction"
 
+MAX_QUESTIONS_PER_FILE = 40
+
 # Note here we have excluded animal_model, engineered_cells, and patient_cells_tissues. We could opt to include
 # them later if we think we have time from the analysts to review. Also note we are including papers and observations
 # here so that we can generate questions for them as well.
@@ -48,7 +50,7 @@ def _get_hpo_reference() -> IFetchHPO:
 
 
 def gen_header(current: int, total: int):
-    return f"Discrepancy Questionnaire ({current} of {total})"
+    return f"Discrepancy Questionnaire ({current} of {total})\n"
 
 
 def gen_group_header(gene: str, pmid: str) -> str:
@@ -64,61 +66,75 @@ def gen_group_header(gene: str, pmid: str) -> str:
         else:
             link = paper.props.get("link", "Unknown link")
 
-    return f'This section contains questions pertaining to the gene {gene} and paper "{title}" ({link}).'
+    return f'The following section contains questions pertaining to the gene {gene} and paper "{title}" ({link}).\n'
 
 
-def gen_paper_finding_question(gene: str, pmid: str) -> str:
+def gen_paper_finding_question(idx: str, gene: str, pmid: str) -> str:
     return (
-        f"The paper {pmid} discusses one or more human genetic variants in the gene {gene}.\n" "A. True\n" "B. False\n"
+        f"\tQ{idx}. The paper {pmid} discusses one or more human genetic variants in the gene {gene}. "
+        "Select one response.\n"
+        "\t\tTrue\n"
+        "\t\tFalse\n"
+        "\t\t[Optional] Note\n"
     )
 
 
-def gen_obs_question(gene: str, pmid: str, hgvs_desc: str, individual_id: str) -> str:
+def gen_obs_question(idx: str, gene: str, pmid: str, hgvs_desc: str, individual_id: str) -> str:
     if individual_id == "inferred proband":
         question_text = (
-            f"The paper {pmid} "
-            f"discusses the variant {hgvs_desc} in {gene} in the primary proband or an unknown/unnamed individual\n"
-            "A. This is completely correct\n"
-            f"B. This variant is discussed, but it is not associated with the gene {gene}\n"
-            "C. This variant is discussed, but it is not possessed by the primary proband "
+            f"\tQ{idx}. The paper {pmid} "
+            f"discusses the variant {hgvs_desc} in {gene} in the primary proband or an unknown/unnamed individual. "
+            "Select one response.\n"
+            "\t\tThis is completely correct\n"
+            f"\t\tThis variant is discussed, but it is not associated with the gene {gene}\n"
+            "\t\tThis variant is discussed, but it is not possessed by the primary proband "
             "or an unknown/unnamed individual\n"
-            "D. This variant is either invalid or it is not discussed in the paper.\n"
+            "\t\tThis variant is either invalid or it is not discussed in the paper\n"
+            "\t\t[Optional] Note\n"
         )
     else:
         question_text = (
-            f"The paper {pmid} "
-            f'discusses the variant {hgvs_desc} in {gene} possessed by the individual "{individual_id}"\n'
-            "A. This is completely correct\n"
-            f"B. This variant is discussed, but it is not associated with the gene {gene}\n"
-            f'C. This variant is discussed, but it is not possessed by the individual "{individual_id}"\n'
-            "D. This variant is either invalid or it is not discussed in the paper.\n"
+            f"\tQ{idx}. The paper {pmid} "
+            f'discusses the variant {hgvs_desc} in {gene} possessed by the individual "{individual_id}". '
+            "Select one response. \n"
+            "\t\tThis is completely correct\n"
+            f"\t\tThis variant is discussed, but it is not associated with the gene {gene}\n"
+            f'\t\tThis variant is discussed, but it is not possessed by the individual "{individual_id}"\n'
+            "\t\tThis variant is either invalid or it is not discussed in the paper.\n"
+            "\t\t[Optional] Note\n"
         )
     return question_text
 
 
-def _model_question(col: str, pmid: str, hgvs_desc: str) -> str:
+def _model_question(idx: str, col: str, pmid: str, hgvs_desc: str) -> str:
     return (
-        f"The paper {pmid} "
-        f"discusses the functional data from '{col.replace('_', ' ')}' for the variant {hgvs_desc}.\n"
-        "A. True, the paper discusses functional data from this source\n"
-        "B. False, the paper does not discuss functional data from this source\n"
+        f"\tQ{idx}. The paper {pmid} "
+        f"discusses the functional data from '{col.replace('_', ' ')}' for the variant {hgvs_desc}. "
+        "Select one response. \n"
+        "\t\tTrue, the paper discusses functional data from this source\n"
+        "\t\tFalse, the paper does not discuss functional data from this source\n"
+        "\t\t[Optional] Note\n"
     )
 
 
 def _phenotype_question(
-    pmid: str, hgvs_desc: str, individual_id: str, output_dict: Dict[str, List[str]], truth_dict: Dict[str, List[str]]
+    idx: str,
+    pmid: str,
+    hgvs_desc: str,
+    individual_id: str,
+    output_dict: Dict[str, List[str]],
+    truth_dict: Dict[str, List[str]],
 ) -> str:
     if individual_id == "inferred proband":
         question_text = (
-            f"The paper {pmid} "
+            f"\tQ{idx}. The paper {pmid} "
             f"discusses the phenotype for the primary proband or an unknown/unnamed individual with "
-            "variant '{hgvs_desc}'\n"
-            "Select all phenotypes possessed by the individual:\n"
+            f"variant '{hgvs_desc}'. Select all phenotypes possessed by the individual:\n"
         )
     else:
         question_text = (
-            f"The paper {pmid} "
-            f"discusses the phenotype for the individual '{individual_id}' with variant '{hgvs_desc}'\n"
+            f"\tQ{idx}. The paper {pmid} "
+            f"discusses the phenotype for the individual '{individual_id}' with variant '{hgvs_desc}'. "
             "Select all phenotypes possessed by the individual:\n"
         )
     spec_terms = set()
@@ -130,107 +146,104 @@ def _phenotype_question(
             spec_terms.add(t)
     if not spec_terms:
         raise ValueError("No HPO terms found for phenotype discrepancy.")
-    letter_prefix = "A"
     hpo_reference = _get_hpo_reference()
     for t in spec_terms:
         if (hpo_term := hpo_reference.fetch(t)) is not None:
-            question_text += f"{letter_prefix}. {hpo_term['name']} ({hpo_term['id']})\n"
-            letter_prefix = chr(ord(letter_prefix) + 1)
-            assert letter_prefix != "["  # too many HPO terms to render.
+            question_text += f"\t\t{hpo_term['name']} ({hpo_term['id']})\n"
+    question_text += "\t\t[Optional] Note\n"
     return question_text
 
 
-def _study_type_question(pmid: str) -> str:
-    question_text = f"What type of study is the paper {pmid}?\n"
-    letter_prefix = "A"
-    for study_type in ["case report", "case series", "cohort analysis", "other"]:
-        question_text += f"{letter_prefix}. {study_type}\n"
-        letter_prefix = chr(ord(letter_prefix) + 1)
+def _study_type_question(idx: str, pmid: str) -> str:
+    question_text = f"\tQ{idx}. What type of study is the paper {pmid}? Select one response.\n"
+    for study_type in ["Case report", "Case series", "Cohort analysis", "Other", "[Optional] Note"]:
+        question_text += f"\t\t{study_type}\n"
     return question_text
 
 
-def _variant_inheritance_question(pmid: str, hgvs_desc: str, individual_id: str) -> str:
+def _variant_inheritance_question(idx: str, pmid: str, hgvs_desc: str, individual_id: str) -> str:
     if individual_id == "inferred proband":
         question_text = (
-            f"The paper {pmid} "
+            f"\tQ{idx}. The paper {pmid} "
             f"discusses the inheritance pattern for the variant '{hgvs_desc}' possessed by the primary proband or an "
-            f"unknown individual. What is the actual inheritance pattern in this case?\n"
+            f"unknown individual. What is the actual inheritance pattern in this case? Select one response.\n"
         )
     else:
         question_text = (
-            f"The paper {pmid} "
+            f"\tQ{idx}. The paper {pmid} "
             f"discusses the inheritance pattern for the variant '{hgvs_desc}' possessed by the individual "
-            f"'{individual_id}'. What is the actual inheritance pattern in this case?\n"
+            f"'{individual_id}'. What is the actual inheritance pattern in this case? Select one response.\n"
         )
-    letter_prefix = "A"
-    for value in ["de novo", "inherited", "unknown"]:
-        question_text += f"{letter_prefix}. {value}\n"
-        letter_prefix = chr(ord(letter_prefix) + 1)
+    for value in ["De novo", "Inherited", "Unknown", "[Optional] Note"]:
+        question_text += f"\t\t{value}\n"
     return question_text
 
 
-def _variant_type_question(pmid: str, hgvs_desc: str) -> str:
+def _variant_type_question(idx: str, pmid: str, hgvs_desc: str) -> str:
     potential_values = (
         [
-            "missense",
-            "stop gained",
-            "stop lost",
-            "splice region",
-            "frameshift",
-            "synonymous",
-            "inframe deletion",
-            "indel",
-            "unknown",
+            "Missense",
+            "Stop gained",
+            "Stop lost",
+            "Splice region",
+            "Frameshift",
+            "Synonymous",
+            "Inframe deletion",
+            "Indel",
+            "Unknown",
+            "[Optional] Note",
         ],
     )
     question_text = (
-        f"The paper {pmid} "
-        f"discusses the variant type for {hgvs_desc}. What is the actual variant type in this case?\n"
+        f"\tQ{idx}. The paper {pmid} "
+        f"discusses the variant type for {hgvs_desc}. What is the actual variant type in this case? "
+        "Select one response.\n"
     )
-    letter_prefix = "A"
     for value in potential_values:
-        question_text += f"{letter_prefix}. {value}\n"
-        letter_prefix = chr(ord(letter_prefix) + 1)
+        question_text += f"\t\t{value}\n"
     return question_text
 
 
-def _zygosity_question(pmid: str, hgvs_desc: str, individual_id: str) -> str:
-    potential_values = ["homozygous", "heterozygous", "compound heterozygous", "unknown"]
+def _zygosity_question(idx: str, pmid: str, hgvs_desc: str, individual_id: str) -> str:
+    potential_values = ["Homozygous", "Heterozygous", "Compound heterozygous", "Unknown", "[Optional] Note"]
 
     if individual_id == "inferred proband":
         question_text = (
-            f"The paper {pmid} "
+            f"\tQ{idx}. The paper {pmid} "
             f"discusses the zygosity for the variant '{hgvs_desc}' possessed by the primary proband or an unknown "
-            "individual. What is the actual zygosity in this case?\n"
+            "individual. What is the actual zygosity in this case? Select one response.\n"
         )
     else:
         question_text = (
-            f"The paper {pmid} "
+            f"\tQ{idx}. The paper {pmid} "
             f"discusses the zygosity for the variant '{hgvs_desc}' possessed by the individual '{individual_id}'. "
-            "What is the actual zygosity in this case?\n"
+            "What is the actual zygosity in this case? Select one response.\n"
         )
-    letter_prefix = "A"
     for value in potential_values:
-        question_text += f"{letter_prefix}. {value}\n"
-        letter_prefix = chr(ord(letter_prefix) + 1)
+        question_text += f"\t\t{value}\n"
     return question_text
 
 
-def gen_content_question(col, params: Dict[str, Any]) -> str:
+def gen_content_question(idx: str, col: str, params: Dict[str, Any]) -> str:
     if col in ["animal_model", "engineered_cells", "patient_cells_tissues"]:
-        return _model_question(col, params["pmid"], params["hgvs_desc"])
+        return _model_question(idx, col, params["pmid"], params["hgvs_desc"])
     elif col in ["phenotype"]:
         return _phenotype_question(
-            params["pmid"], params["hgvs_desc"], params["individual_id"], params["output_dict"], params["truth_dict"]
+            idx,
+            params["pmid"],
+            params["hgvs_desc"],
+            params["individual_id"],
+            params["output_dict"],
+            params["truth_dict"],
         )
     elif col in ["study_type"]:
-        return _study_type_question(params["pmid"])
+        return _study_type_question(idx, params["pmid"])
     elif col in ["variant_inheritance", "zygosity"]:
-        return _variant_inheritance_question(params["pmid"], params["hgvs_desc"], params["individual_id"])
+        return _variant_inheritance_question(idx, params["pmid"], params["hgvs_desc"], params["individual_id"])
     elif col in ["variant_type"]:
-        return _variant_type_question(params["pmid"], params["hgvs_desc"])
+        return _variant_type_question(idx, params["pmid"], params["hgvs_desc"])
     elif col in ["zygosity"]:
-        return _zygosity_question(params["pmid"], params["hgvs_desc"], params["individual_id"])
+        return _zygosity_question(idx, params["pmid"], params["hgvs_desc"], params["individual_id"])
     else:
         raise ValueError(f"Unknown column: {col}")
 
@@ -308,18 +321,19 @@ for file_number, group_df in discrepancies.groupby("file_number"):
     # Not the cleanest way to do this, but it works.
     file_number_int = int(group_df["file_number"].iloc[0])
 
-    with open(os.path.join(OUTPUT_DIR, f"discrepancies_qs_{file_number}.txt"), "w") as f:
-        f.write(f"# {gen_header(file_number_int + 1, n_files)} #\n\n\n")
+    with open(os.path.join(OUTPUT_DIR, f"discrepancies_qs_{file_number}.tsv"), "w") as f:
+        f.write(f"{gen_header(file_number_int + 1, n_files)}\n")
 
         for (gene, pmid), paper_df in group_df.groupby(["gene", "pmid"]):
-            f.write(f"## {gen_group_header(gene, pmid)} ##\n\n\n")
+            f.write(f"{gen_group_header(gene, pmid)}\n\n")
             for idx, row in paper_df.iterrows():
+                idx_str = str(idx)
                 if row["task"] == "papers":
-                    q = gen_paper_finding_question(row["gene"], row["pmid"])
+                    q = gen_paper_finding_question(idx_str, row["gene"], row["pmid"])
                 elif row["task"] == "observations":
-                    q = gen_obs_question(row["gene"], row["pmid"], row["hgvs_desc"], row["individual_id"])
+                    q = gen_obs_question(idx_str, row["gene"], row["pmid"], row["hgvs_desc"], row["individual_id"])
                 else:
-                    q = gen_content_question(row["task"], row.to_dict())
-                f.write(f"Q{idx}. {q}\n\n")
+                    q = gen_content_question(idx_str, row["task"], row.to_dict())
+                f.write(f"{q}\n")
 
 # %% Intentionally empty.
