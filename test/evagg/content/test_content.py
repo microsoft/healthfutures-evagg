d3da9adaf4c05f3c9c2575ea326ef1aa04ca4ef3
Miah Wander <miah@microsoft.com>
2024-10-16 19:07:37 +0000

    completed content tests and coverage

--- test/evagg/content/test_content.py
+++ test/evagg/content/test_content.py
@@ -1,10 +1,13 @@
 import json
+import os
+import tempfile
 from typing import Any
+from unittest.mock import patch
 
 import pytest
 
 from lib.evagg import PromptBasedContentExtractor
-from lib.evagg.content import IFindObservations, Observation, TextSection
+from lib.evagg.content import IFindObservations, Observation, PromptBasedContentExtractorCached, TextSection
 from lib.evagg.content.fulltext import get_fulltext
 from lib.evagg.llm import IPromptClient
 from lib.evagg.ref import IFetchHPO, ISearchHPO
@@ -35,6 +38,7 @@ def mock_phenotype_searcher(mock_client: type) -> ISearchHPO:
 def paper() -> Paper:
     return Paper(
         id="12345678",
+        pmid="12345678",
         citation="Doe, J. et al. Test Journal 2021",
         title="Test Paper Title",
         abstract="This the abstract from a test paper.",
@@ -576,6 +580,322 @@ def test_prompt_based_content_extractor_json_prompt_response(
     assert content[0]["zygosity"] == '{"key": "value"}'
 
 
+def test_prompt_based_content_extractor_functional_study(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    fields = {
+        "gene": "CHI3L1",
+        "paper_id": "12345678",
+        "hgvs_c": "c.1234A>G",
+        "paper_variant": "c.1234A>G",
+        "individual_id": "unknown",
+        "engineered_cells": "True",
+        "patient_cells_tissues": "True",
+        "animal_model": "False",
+    }
+
+    observation = Observation(
+        variant=HGVSVariant(
+            hgvs_desc=fields["hgvs_c"],
+            gene_symbol=fields["gene"],
+            refseq="transcript",
+            refseq_predicted=True,
+            valid=True,
+            validation_error="",
+            protein_consequence=None,
+            coding_equivalents=[],
+        ),
+        individual="unknown",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=fields["paper_variant"].split(", "),
+        patient_descriptions=["unknown"],
+        paper_id=fields["paper_id"],
+    )
+
+    prompts = mock_prompt(
+        json.dumps({"functional_study": ["cell line", "patient cells"]}),
+        json.dumps({"functional_study": ["patient cells"]}),
+        json.dumps({"functional_study": ["none"]}),
+    )
+    content_extractor = PromptBasedContentExtractor(
+        list(fields.keys()),
+        prompts,
+        mock_observation([observation]),
+        mock_phenotype_searcher(),
+        mock_phenotype_fetcher(),
+    )
+    content = content_extractor.extract(paper, fields["gene"])
+
+    assert len(content) == 1
+    for key in fields:
+        assert content[0][key] == fields[key]
+
+
+def test_prompt_based_content_extractor_field_caching_phenotype(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    paper_id = "12345678"
+    phenotype = "test i1 (HP:0123)"
+
+    variant1 = HGVSVariant(
+        hgvs_desc="c.1234A>G",
+        gene_symbol="CHI3L1",
+        refseq="transcript",
+        refseq_predicted=True,
+        valid=True,
+        validation_error="",
+        protein_consequence=None,
+        coding_equivalents=[],
+    )
+
+    variant2 = HGVSVariant(
+        hgvs_desc="c.4321G>T",
+        gene_symbol="CHI3L1",
+        refseq="transcript",
+        refseq_predicted=True,
+        valid=True,
+        validation_error="",
+        protein_consequence=None,
+        coding_equivalents=[],
+    )
+
+    observation1 = Observation(
+        variant=variant1,
+        individual="I-1",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.1234A>G"],
+        patient_descriptions=["I-1"],
+        paper_id=paper_id,
+    )
+
+    observation2 = Observation(
+        variant=variant2,
+        individual="I-1",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.4321G>T"],
+        patient_descriptions=["I-1"],
+        paper_id=paper_id,
+    )
+
+    prompts = mock_prompt(
+        json.dumps({"phenotypes": ["test"]}),  # phenotypes_all, only one text, so only once.
+        json.dumps({"phenotypes": ["test"]}),  # phenotypes_observation, only one text, so only once.
+        json.dumps({"phenotypes": ["test"]}),  # phenotypes_acronyms, only one text, so only once.
+        json.dumps({"match": "test i1 (HP:0123)"}),
+    )
+    observation_finder = mock_observation([observation1, observation2])
+
+    pheno_searcher = mock_phenotype_searcher(
+        [{"id": "HP:0123", "name": "test i1", "definition": "test", "synonyms": "test"}],
+    )
+    pheno_fetcher = mock_phenotype_fetcher()
+    content_extractor = PromptBasedContentExtractor(
+        ["phenotype"], prompts, observation_finder, pheno_searcher, pheno_fetcher
+    )
+    content = content_extractor.extract(paper, "CHI3L1")
+
+    assert len(content) == 2
+    assert content[0]["phenotype"] == phenotype
+    assert content[1]["phenotype"] == phenotype
+
+
+def test_prompt_based_content_extractor_field_caching_variant_type(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    paper_id = "12345678"
+    variant_type = "missense"
+
+    variant = HGVSVariant(
+        hgvs_desc="c.1234A>G",
+        gene_symbol="CHI3L1",
+        refseq="transcript",
+        refseq_predicted=True,
+        valid=True,
+        validation_error="",
+        protein_consequence=None,
+        coding_equivalents=[],
+    )
+
+    observation1 = Observation(
+        variant=variant,
+        individual="I-1",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.1234A>G"],
+        patient_descriptions=["I-1"],
+        paper_id=paper_id,
+    )
+
+    observation2 = Observation(
+        variant=variant,
+        individual="I-2",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.1234A>G"],
+        patient_descriptions=["I-2"],
+        paper_id=paper_id,
+    )
+    prompts = mock_prompt(
+        # o1 variant_type
+        json.dumps({"variant_type": variant_type}),
+    )
+    observation_finder = mock_observation([observation1, observation2])
+    pheno_searcher = mock_phenotype_searcher()
+    pheno_fetcher = mock_phenotype_fetcher()
+    content_extractor = PromptBasedContentExtractor(
+        ["variant_type"], prompts, observation_finder, pheno_searcher, pheno_fetcher
+    )
+    content = content_extractor.extract(paper, "CHI3L1")
+
+    assert len(content) == 2
+    assert content[0]["variant_type"] == variant_type
+    assert content[1]["variant_type"] == variant_type
+
+
+def test_prompt_based_content_extractor_field_caching_study_type(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    paper_id = "12345678"
+    study_type = "case study"
+
+    variant1 = HGVSVariant(
+        hgvs_desc="c.1234A>G",
+        gene_symbol="CHI3L1",
+        refseq="transcript",
+        refseq_predicted=True,
+        valid=True,
+        validation_error="",
+        protein_consequence=None,
+        coding_equivalents=[],
+    )
+
+    variant2 = HGVSVariant(
+        hgvs_desc="c.4321T>G",
+        gene_symbol="CHI3L1",
+        refseq="transcript",
+        refseq_predicted=True,
+        valid=True,
+        validation_error="",
+        protein_consequence=None,
+        coding_equivalents=[],
+    )
+
+    observation1 = Observation(
+        variant=variant1,
+        individual="I-1",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.1234A>G"],
+        patient_descriptions=["I-1"],
+        paper_id=paper_id,
+    )
+
+    observation2 = Observation(
+        variant=variant2,
+        individual="I-2",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.4321T>G"],
+        patient_descriptions=["I-2"],
+        paper_id=paper_id,
+    )
+    prompts = mock_prompt(
+        json.dumps({"study_type": study_type}),
+    )
+    observation_finder = mock_observation([observation1, observation2])
+    pheno_searcher = mock_phenotype_searcher()
+    pheno_fetcher = mock_phenotype_fetcher()
+    content_extractor = PromptBasedContentExtractor(
+        ["study_type"], prompts, observation_finder, pheno_searcher, pheno_fetcher
+    )
+    content = content_extractor.extract(paper, "CHI3L1")
+
+    assert len(content) == 2
+    assert content[0]["study_type"] == study_type
+    assert content[1]["study_type"] == study_type
+
+
+def test_prompt_based_content_extractor_unprocessable_paper(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    paper.props["can_access"] = False
+    content_extractor = PromptBasedContentExtractor(
+        [], mock_prompt({}), mock_observation([]), mock_phenotype_searcher, mock_phenotype_fetcher
+    )
+    content = content_extractor.extract(paper, "CHI3L1")
+    assert content == []
+
+
+def test_prompt_based_content_extractor_no_observations(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    content_extractor = PromptBasedContentExtractor(
+        [], mock_prompt({}), mock_observation([]), mock_phenotype_searcher, mock_phenotype_fetcher
+    )
+    content = content_extractor.extract(paper, "CHI3L1")
+    assert content == []
+
+
+def test_caching(
+    paper: Paper, mock_prompt: Any, mock_observation: Any, mock_phenotype_searcher: Any, mock_phenotype_fetcher: Any
+) -> None:
+    study_type = "case study"
+    gene = "CHI3L1"
+
+    observation = Observation(
+        variant=HGVSVariant(
+            hgvs_desc="c.1234A>G",
+            gene_symbol=gene,
+            refseq="transcript",
+            refseq_predicted=True,
+            valid=True,
+            validation_error="",
+            protein_consequence=None,
+            coding_equivalents=[],
+        ),
+        individual="I-1",
+        texts=[TextSection("TEST", "test", 0, "Here is the observation text.", "unknown")],
+        variant_descriptions=["c.1234A>G"],
+        patient_descriptions=["I-1"],
+        paper_id=paper.id,
+    )
+
+    prompts = mock_prompt(
+        json.dumps({"study_type": study_type}),
+    )
+    observation_finder = mock_observation([observation])
+    pheno_searcher = mock_phenotype_searcher()
+    pheno_fetcher = mock_phenotype_fetcher()
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        # Mock get_run_path to return the temporary directory.
+        with patch("lib.evagg.utils.cache.get_run_path", return_value=tmpdir):
+
+            # verify no cache exists.
+            assert not os.path.exists(
+                os.path.join(
+                    tmpdir, "results_cache", "PromptBasedContentExtractor", f"extract_{paper.props['pmid']}_{gene}.json"
+                )
+            )
+            content_extractor = PromptBasedContentExtractorCached(
+                ["study_type"], prompts, observation_finder, pheno_searcher, pheno_fetcher
+            )
+            content = content_extractor.extract(paper, gene)
+
+            assert len(content) == 1
+            assert content[0]["study_type"] == study_type
+
+            # verify cache was created.
+            assert os.path.exists(
+                os.path.join(
+                    tmpdir, "results_cache", "PromptBasedContentExtractor", f"extract_{paper.props['pmid']}_{gene}.json"
+                )
+            )
+
+            # The injected dependencies will be exhausted, so if we don't use the cache, we'll get an error.
+            content = content_extractor.extract(paper, gene)
+
+            assert len(content) == 1
+            assert content[0]["study_type"] == study_type
+
+
 xmldoc = """
     <document>
         <id>7933980</id>
@@ -584,7 +904,7 @@ xmldoc = """
 """
 
 
-def test_fulltext():
+def test_fulltext() -> None:
     xml1 = """
         <passage>
             <infon key="section_type">TITLE</infon>
@@ -600,7 +920,7 @@ def test_fulltext():
     assert get_fulltext(xmldoc.format(content=xml1), include=["TITLE"], exclude=["TITLE"]) == ""
 
 
-def test_fulltext_missing():
+def test_fulltext_missing() -> None:
     xml1 = """
         <passage>
         </passage>
@@ -624,4 +944,7 @@ def test_fulltext_missing():
     assert str(e.value) == "Missing 'type' infon element in passage for document 7933980"
     with pytest.raises(ValueError) as e:
         get_fulltext(xmldoc.format(content=xml3))
+    assert str(e.value) == "Missing 'offset' infon element in TITLE passage for document 7933980"
+    assert str(e.value) == "Missing 'offset' infon element in TITLE passage for document 7933980"
+    assert str(e.value) == "Missing 'offset' infon element in TITLE passage for document 7933980"
     assert str(e.value) == "Missing 'offset' infon element in TITLE passage for document 7933980"
