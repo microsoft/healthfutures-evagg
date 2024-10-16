c02608923e61849ecd0f7ed645246e95557d6835
Greg Smith <gregsmi@microsoft.com>
2024-10-16 08:56:34 -0700

    fixed test run trigger

--- test/evagg/utils/test_git.py
+++ test/evagg/utils/test_git.py
@@ -1,16 +1,20 @@
-import pytest
 from unittest.mock import patch
+
+import pytest
 
 from lib.evagg.utils.git import GitError, ModifiedFile, RepoStatus
 
 
-@pytest.mark.parametrize("status, type, name", [
-    ("1 .M N... 100644 100644 100644 hash1 hash2 lib/evagg/changed.py", "changed", "lib/evagg/changed.py"),
-    ("2 .M N... 100644 100644 100644 hash1 hash2 R100 lib/evagg/to.py\tlib/evagg/from.py", "renamed", "lib/evagg/to.py"),
-    ("u .M N... 100644 100644 100644 hash1 hash2 hash3 hash4 lib/evagg/u.py", "unmerged", "lib/evagg/u.py"),
-    ("? lib/evagg/new.py", "untracked", "lib/evagg/new.py"),
-    ("! lib/evagg/i.py", "ignored", "lib/evagg/i.py"),
-])
+@pytest.mark.parametrize(
+    "status, type, name",
+    [
+        ("1 .M N... 100644 100644 100644 hash1 hash2 lib/evagg/changed.py", "changed", "lib/evagg/changed.py"),
+        ("2 .M N... 100644 100644 100644 hash1 hash2 R100 lib/evagg/to.py\tlib/evagg/from.py", "renamed", "lib/evagg/to.py"),
+        ("u .M N... 100644 100644 100644 hash1 hash2 hash3 hash4 lib/evagg/u.py", "unmerged", "lib/evagg/u.py"),
+        ("? lib/evagg/new.py", "untracked", "lib/evagg/new.py"),
+        ("! lib/evagg/i.py", "ignored", "lib/evagg/i.py"),
+    ]
+)
 def test_parse_modified_file(status, type, name):
     assert ModifiedFile(status).type == type
     assert ModifiedFile(status).name == name
