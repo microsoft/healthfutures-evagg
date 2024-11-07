50d41a351e43f89f2bc7f8da9197b215ae3b08d0
Miah Wander <miah@microsoft.com>
2024-11-07 19:02:16 +0000

    sneaking in a change to unbreak the ci build

--- lib/evagg/ref/ncbi.py
+++ lib/evagg/ref/ncbi.py
@@ -1,6 +1,5 @@
 import logging
 import urllib.parse as urlparse
-from abc import ABC
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
 
 from defusedxml import ElementTree
@@ -36,7 +35,7 @@ class NcbiApiSettings(SettingsModel, extra=Extra.forbid):
         return values
 
 
-class NcbiClientBase(ABC):
+class NcbiClientBase:
     EUTILS_HOST = "https://eutils.ncbi.nlm.nih.gov"
     EUTILS_SEARCH_SITE = "/entrez/eutils/esearch.fcgi"
     EUTILS_FETCH_SITE = "/entrez/eutils/efetch.fcgi"
