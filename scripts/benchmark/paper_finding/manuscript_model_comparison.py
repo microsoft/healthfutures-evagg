f3b0cffffd401e2aec57ebb62541cdc46fca4f5d
Miah Wander <miah@microsoft.com>
2024-12-04 23:00:25 +0000

    update papers model comparison

--- scripts/benchmark/paper_finding/manuscript_model_comparison.py
+++ scripts/benchmark/paper_finding/manuscript_model_comparison.py
@@ -6,6 +6,7 @@ in the `MODELS` constant below.
 
 # %% Imports.
 
+import json
 from typing import Dict
 
 import matplotlib.pyplot as plt
@@ -35,10 +36,27 @@ for model in MODELS:
 # Concatenate the dataframes.
 data = pd.concat([data_dicts[model][split] for model in MODELS for split in ["train", "test"]])
 
+# %% Incorporate runtimes and cost estimates.
+
+INVALID_RUNTIME_IDS = ["20240911_223218", "20240920_223702"]
+
+
+def get_runtime_sec(run_id: str) -> float:
+
+    metadata = json.load(open(f".out/run_evagg_pipeline_{run_id}/run.json"))
+    return metadata["elapsed_secs"]
+
+
+data["runtime"] = data.run_id.apply(get_runtime_sec)
+
+# Set the runtimes for the invalid runs to NaN.
+data.loc[data.run_id.isin(INVALID_RUNTIME_IDS), "runtime"] = None
+
+
 # %% Make plots.
 
 # Make a bar plot for precision on the test set where there is a different bar for each model, and std dev error bars.
-for metric in ["precision", "recall"]:
+for metric in ["precision", "recall", "f1"]:
     for split in ["train", "test"]:
         plt.figure(figsize=(6, 6))
         sns.barplot(
@@ -55,10 +73,11 @@ for metric in ["precision", "recall"]:
 # %% Print out summary statistics.
 
 # Make a table that averages across runs, stratified on model and split, giving average precision and recall for each.
-summary = data.groupby(["model", "split"])[["precision", "recall"]].agg(["mean", "std"]).reset_index()
+summary = data.groupby(["model", "split"])[["precision", "recall", "runtime"]].agg(["mean", "std"]).reset_index()
 
 # Just display to 3 sig figs
 summary = summary.round(3)
 print(summary)
+
 
 # %% Intentionally empty.
