921d598fa4fb91b46ed62f714703c71f68e29c61
Miah Wander <miah@microsoft.com>
2024-12-05 22:55:51 +0000

    update model comparisons

--- scripts/benchmark/paper_finding/manuscript_model_comparison.py
+++ scripts/benchmark/paper_finding/manuscript_model_comparison.py
@@ -12,6 +12,8 @@ from typing import Dict
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from statsmodels.stats.anova import AnovaRM
+from statsmodels.stats.multicomp import MultiComparison
 
 # %% Constants.
 
@@ -56,7 +58,7 @@ data.loc[data.run_id.isin(INVALID_RUNTIME_IDS), "runtime"] = None
 # %% Make plots.
 
 # Make a bar plot for precision on the test set where there is a different bar for each model, and std dev error bars.
-for metric in ["precision", "recall", "f1"]:
+for metric in ["precision", "recall"]:
     for split in ["train", "test"]:
         plt.figure(figsize=(6, 6))
         sns.barplot(
@@ -68,6 +70,40 @@ for metric in ["precision", "recall", "f1"]:
         )
         plt.title(f"Paper finding {metric} [{split}]")
         plt.ylim(0, 1)
+
+# Make a bar plot that combines train and test into a single plot
+for metric in ["precision", "recall"]:
+    plt.figure(figsize=(6, 6))
+    sns.barplot(
+        data=data,
+        x="model",
+        y=metric,
+        errorbar="sd",
+        hue="split",
+    )
+    plt.title(f"Model comparison - Paper finding {metric}")
+    plt.ylim(0.6, 1)
+
+# %% Detailed stats.
+
+# Test for a statistical effect of model using a 2-way anova with repeated measures
+# The factors are model and split, the dependent variable is recall.
+
+
+fit_data = data[["recall", "model", "split"]].copy()
+fit_data.reset_index(names="subject", inplace=True)
+
+aovrm = AnovaRM(fit_data, "recall", "subject", within=["model", "split"])
+res = aovrm.fit()
+
+# Print the results
+print(res)
+
+# Since model is significant, we can do a post-hoc comparison using Tukey's HSD test.
+
+mc = MultiComparison(fit_data["recall"], fit_data["model"])
+result = mc.tukeyhsd()
+print(result)
 
 
 # %% Print out summary statistics.
