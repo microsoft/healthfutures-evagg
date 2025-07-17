c22fbdb4628c8967888d9f842ed48a8dc6e1558c
Leonhard Gruenschloss <l.gruenschloss@vcgs.org.au>
2025-07-17 15:32:21 +1000

    Add cache dir utility

--- lib/evagg/utils/__init__.py
+++ lib/evagg/utils/__init__.py
@@ -1,10 +1,13 @@
 """Package for utilities."""
 
+from .cache_dirs import CacheDirectoryManager
 from .logging import init_logger
 from .settings import get_azure_credential, get_dotenv_settings, get_env_settings
 from .web import CosmosCachingWebClient, IWebContentClient, RequestsWebContentClient
 
 __all__ = [
+    # Cache.
+    "CacheDirectoryManager",
     # Settings.
     "get_azure_credential",
     "get_dotenv_settings",
